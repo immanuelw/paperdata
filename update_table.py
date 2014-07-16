0c6c0d58accf622f3c88d3a80a6fbe3f700e4f3b
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-07-16 13:56:52 -0400

    Adding more prototype stuff

--- update_table.py
+++ update_table.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Load data into MySQL table 
+# Loa data into MySQL table 
 
 # import the MySQLdb and sys modules
 import MySQLdb
 import sys
 import getpass
 
-table = raw_input('Load data into table named:')
 datab = raw_input('Database:')
+table = raw_input('Update data in table named:')
 pswd = getpass.getpass('Password:')
 qry = sys.argv[1]
 # open a database connection
@@ -21,11 +21,12 @@ cursor = connection.cursor()
 print sys.argv[1]
 # execute the SQL query using execute() method.
 cursor.execute('''USE paperdata;
-LOAD DATA LOCAL INFILE '%s' INTO TABLE %s
-COLUMNS TERMINATED BY ','
-LINES TERMINATED BY '\n' '''%(sys.argv[1], table]))
-
-print 'Table data loaded.'
+UPDATE %s
+SET %s = %s
+WHERE %s = %s;
+'''%(table, compressed, compr_value, obs_string, obsnum)) 
+###change so %d if number or %s if string entry!!!
+print 'Table data updated.'
 
 # close the cursor object
 cursor.close()
