a1a2c3bf4b6296957a0c784d9d10f8618c834d21
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-07-17 12:28:08 -0400

    Prototype for perpetual versions

--- always_update_database.py
+++ always_update_database.py
@@ -33,90 +33,93 @@ def complete_check(count_jday, count_complete):
                         WHERE %s = %d;
                         '''%(table, tape, ready_to_tape, obsnum_string, obsnum))
 
-#need way to get compr_value and obsnum from paperdistiller 
+#Perpetually check paperdistiller database for updates
+while True:
 
-# open a database connection
-connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = 'paperdistiller', local_infile=True)
+	#need way to get compr_value and obsnum from paperdistiller 
 
-cursor = connection.cursor()
+	# open a database connection
+	connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = 'paperdistiller', local_infile=True)
 
-# execute the SQL query using execute() method.
-cursor.execute('SELECT obsnum, status, julian_date from observations order by julian_date')
+	cursor = connection.cursor()
 
-#collects information from query
-results = cursor.fetchall()
+	# execute the SQL query using execute() method.
+	cursor.execute('SELECT obsnum, status, julian_date from observations order by julian_date')
 
-#close, save and end connection
-cursor.close()
-connection.commit()
-connection.close()
+	#collects information from query
+	results = cursor.fetchall()
 
-# open a database connection
-# be sure to change the host IP address, username, password and database name to match your own
-connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = datab, local_infile=True)
+	#close, save and end connection
+	cursor.close()
+	connection.commit()
+	connection.close()
 
-# prepare a cursor object using cursor() method
-cursor = connection.cursor()
+	# open a database connection
+	# be sure to change the host IP address, username, password and database name to match your own
+	connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = datab, local_infile=True)
 
-#results is a list of lists
-for items in results:
+	# prepare a cursor object using cursor() method
+	cursor = connection.cursor()
 
-	count_jday = len(jday_results)
-	#sets value of initial julian day found
-	if count_jday == 0:
-		julian_day = int(str(items[2])[3:7]) #error if psa32
+	#results is a list of lists
+	for items in results:
+
+		count_jday = len(jday_results)
+		#sets value of initial julian day found
+		if count_jday == 0:
+			julian_day = int(str(items[2])[3:7]) #error if psa32
 	
-        obsnum = items[0]
+	        obsnum = items[0]
 
-	j_day = int(str(items[2])[3:7])
+		j_day = int(str(items[2])[3:7])
 
-	#counts amount of files with same Julian Day
-	if j_day == julian_day:
-		jday_results.append(items)
-		#checks if file is done compression
-	        if items[1] == 'COMPLETE':
-	                compr_value = True
-			#counts amount of files complete
-			count_complete += 1
-	        else:
-	                compr_value = False
+		#counts amount of files with same Julian Day
+		if j_day == julian_day:
+			jday_results.append(items)
+			#checks if file is done compression
+		        if items[1] == 'COMPLETE':
+		                compr_value = True
+				#counts amount of files complete
+				count_complete += 1
+		        else:
+		                compr_value = False
 
-		# execute the SQL query using execute() method.
-		cursor.execute('''
-		UPDATE %s
-		SET %s = %s
-		WHERE %s = %d;
-		'''%(table, compressed, compr_value, obsnum_string, obsnum)) 
-		###change so %d if number or %s if string entry!!!
-	else:
-		complete_check(count_jday, count_complete)	
-		jday_results = []
-		jday_results.append(items)
-		julian_day = j_day
-		count_jday = 0
-		count_complete = 0
-		items[1] == 'COMPLETE':
-                        compr_value = True
-                        count_complete += 1
-                else:
-                        compr_value = False
+			# execute the SQL query using execute() method.
+			cursor.execute('''
+			UPDATE %s
+			SET %s = %s
+			WHERE %s = %d;
+			'''%(table, compressed, compr_value, obsnum_string, obsnum)) 
+			###change so %d if number or %s if string entry!!!
+		else:
+			complete_check(count_jday, count_complete)	
+			jday_results = []
+			jday_results.append(items)
+			julian_day = j_day
+			count_jday = 0
+			count_complete = 0
+			items[1] == 'COMPLETE':
+	                        compr_value = True
+	                        count_complete += 1
+	                else:
+	                        compr_value = False
 
-                cursor.execute('''
-                UPDATE %s
-                SET %s = %s
-                WHERE %s = %d;
-                '''%(table, compressed, compr_value, obsnum_string, obsnum))
+	                cursor.execute('''
+	                UPDATE %s
+	                SET %s = %s
+	                WHERE %s = %d;
+	                '''%(table, compressed, compr_value, obsnum_string, obsnum))
 
-print 'Table data updated.'
+	#print 'Table data updated.'
 
-# close the cursor object
-cursor.close()
+	# close the cursor object
+	cursor.close()
 
-#save changes to database
-connection.commit()
+	#save changes to database
+	connection.commit()
 
-# close the connection
-connection.close()
+	# close the connection
+	connection.close()
 
 # exit the program
 sys.exit()
