93551e81441c26938e0979d3e605e13274b0dc0f
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-07-30 15:17:35 -0400

    Quick backup of obsnums

--- obsnum_logger.py
+++ obsnum_logger.py
@@ -8,7 +8,8 @@ import sys
 import getpass
 import csv
 
-table = 'paperdata'
+#table = 'paperdata'
+table = raw_input('Input table name: ')
 usrnm = raw_input('Root username: ')
 pswd = getpass.getpass('Root password: ')
 
@@ -16,7 +17,10 @@ raw = 'raw_location'
 obsnum_string = 'obsnum'
 delt = 'ready_to_delete'
 
-resultFile = open('/data2/home/immwa/scripts/paper/jd_obsnum.csv' ,'wb')
+#filename = '/data2/home/immwa/scripts/paper/jd_obsnum.csv'
+filename = '/data2/home/immwa/scripts/paper/jd_obsnum_%s.csv'%(table)
+
+resultFile = open(filename,'wb')
 wr = csv.writer(resultFile, dialect='excel')
 
 # open a database connection
@@ -27,7 +31,9 @@ connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db
 cursor = connection.cursor()
 
 #execute MySQL query
-cursor.execute('SELECT julian_day, obsnum from paperdata order by julian_day')
+#cursor.execute('SELECT julian_date, obsnum from paperdata order by julian_day')
+
+cursor.execute('SELECT julian_date, obsnum from %s order by julian_date' %(table))
 
 #collects information from query
 results = cursor.fetchall()
