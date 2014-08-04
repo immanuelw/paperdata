57fbc7a059d9ea0ac343d726a8de893cf0ec7d46
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-08-04 11:59:40 -0400

    Fast pull from paperdistiller

---
+++ load_database_from_paperdistiller.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Load data into MySQL table 
+
+# import the MySQLdb and sys modules
+import MySQLdb
+import sys
+import getpass
+import os
+import inspect
+import csv
+
+#Functions which simply find the file size of the .uvcRRE files
+def get_size(start_path):
+	total_size = 0
+	for dirpath, dirnames, filenames in os.walk(start_path):
+		for f in filenames:
+			fp = os.path.join(dirpath, f)
+			total_size += os.path.getsize(fp)
+	return total_size
+
+def sizeof_fmt(num):
+	for x in ['bytes','KB','MB','GB']:
+		if num < 1024.0:
+			return "%3.1f%s" % (num, x)
+		num /= 1024.0
+	return "%3.1f%s" % (num, 'TB')
+
+#User input information
+datab = 'paperdata'
+usrnm = raw_input('Username: ')
+pswd = getpass.getpass('Password: ')
+
+paperd = 'paperdistiller'
+
+db128 = '/data2/home/immwa/scripts/paper_output/db_output128PD.csv'
+
+host = 'folio'
+
+#searches for only particular files
+dbnum = db128
+
+#combined all eras into one table
+table_name = 'paperdata'
+
+resultFile = open(dbnum,'wb')
+
+#create 'writer' object
+wr = csv.writer(resultFile, dialect='excel')
+
+#pulls all relevant information from full paperdistiller database
+
+connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdistiller', local_infile=True)
+
+# prepare a cursor object using cursor() method
+cursor = connection.cursor()
+
+# execute the SQL query using execute() method.
+cursor.execute('SELECT filename, obsnum from file order by obsnum')
+
+results = cursor.fetchall()
+
+for item in results:
+	cursor.execute('SELECT julian_date, pol, length where obsnum = %d'%(item[1])
+
+sec_results = cursor.fetchall()
+# close the cursor object
+cursor.close()
+
+#save changes to database
+connection.commit()
+
+# close the connection
+connection.close()
+
+
+#gather all the the data to input into paperdata
+
+for item in results:
+	item += sec_results[0]
+	item += sec_results[1]
+	item += sec_results[2]
+
+print item
+
+#results list of lists should contain path, obsnum, julian_date, polarization string, and length
+
+for item in results:
+	#indicates location of raw file (usually same directory as compressed) FIX TO MAKE COMPRESSED	
+	path = results[0]
+
+	#indicates size of file
+	sz = sizeof_fmt(get_size(path))
+
+	#checks if any data is in file
+	visdata = os.path.join(path, 'visdata')
+	if not os.path.isfile(visdata):
+		continue
+
+        #allows uv access
+	try:
+	        uv = A.miriad.UV(path)
+	except:
+		continue	
+
+	#indicates julian date
+	jdate = results[2]
+
+	#indicates julian day
+	jday = int(str(jdate)[3:7])	
+
+	#indicates set of data used
+	era = 128
+
+	#indicates type of file in era
+	era_type = 'NULL'
+
+	#assign letters to each polarization
+	polarization = results[3]
+
+	#indicates length of information in file
+	length = results[4]
+
+	#indicates obsnum
+	obsnum = results[1]
+
+	#location of raw files
+	raw_location = path #do not know where they are for any of them yet
+
+	#location of calibrate files
+	cal_location = 'NULL'
+
+	#indicates if file is compressed
+	compressed = 1
+
+	#shows location of raw data on tape
+	tape_location = 'NULL'
+
+	#variable indicating if all files have been successfully compressed in one day
+	ready_to_tape = 0
+
+	#indicates if all raw data is compressed, moved to tape, and the raw data can be deleted from folio
+	delete_file = 0 
+
+	#create list of important data and open csv file
+	databs = [[host,path,era,era_type,obsnum,jday,jdate,polarization,length,raw_location,cal_location,tape_location,str(sz),compressed,ready_to_tape,delete_file]]
+	print databs 
+
+	#write to csv file by item in list
+	for item in databs:
+		wr.writerow(item)
+
+#Load data into named database and table
+
+# open a database connection
+# be sure to change the host IP address, username, password and database name to match your own
+connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdata', local_infile=True)
+
+# prepare a cursor object using cursor() method
+cursor = connection.cursor()
+
+print dbnum 
+# execute the SQL query using execute() method.
+cursor.execute('''USE paperdata;
+LOAD DATA LOCAL INFILE '%s' INTO TABLE paperdata
+COLUMNS TERMINATED BY ','
+LINES TERMINATED BY '\n' '''%(dbnum))
+
+print 'Table data loaded.'
+
+# close the cursor object
+cursor.close()
+
+#save changes to database
+connection.commit()
+
+# close the connection
+connection.close()
+
+# exit the program
+sys.exit()
