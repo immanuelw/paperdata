423eb35453f7a862971b3bde4f71edd4c6023819
Immanuel Washington <immwa@node02.(none)>
2014-09-08 12:22:23 -0400

    Added exceptions, completed method of renaming files with little overhead

--- rename.py
+++ rename.py
@@ -15,8 +15,8 @@ beg = 'zen'
 dot = '.'
 
 #location of directory searched
-#data = '/mnt/USB/0/D40P01/Good/FOUND.ODR/Orphan9/'
-data = '/data4/paper/file_renaming_test_output/'
+data = '/mnt/USB/0/D40P01/Good/FOUND.ODR/Orphan9/'
+#data = '/data4/paper/file_renaming_test_output/'
 
 #location of directory to move to
 datashift = '/data4/paper/file_renaming_test_output_complete/'
@@ -34,10 +34,14 @@ def get_size(start_path):
                         total_size += os.path.getsize(fp)
         return total_size
 
+#dummy count variable
+count = 0
+
 #loop over files/folders to look through
 for root, dirs, files in os.walk(data):
 	for dir in dirs:
 		for file in files:
+			count += 1
 			#print dir
 			dummy = os.path.join(root,dir)
 			data_file = os.path.join(dummy,file)
@@ -45,16 +49,25 @@ for root, dirs, files in os.walk(data):
 			#check if file size is over 100MB, if not: skip
 			if get_size(data_file) < 104857600:
 				continue
+
+			#if over 100MB, copy over to folio/copy to new folder and rename
+			try:
+				shutil.copy(data_file, '/data4/paper/file_renaming_test_output/%d/visdata' %(count))
+			except:
+				print 'Directory /data4/paper/file_renaming_test_output/%d/ doesnt exist' %(count)
+
+			#set string to location of new .uv file
+			newUV = '/data4/paper/file_renaming_test_output/%d' %(count)
+
 			#allows uv access
 			try:
-				uv = A.miriad.UV(data_file)
+				uv = A.miriad.UV(newUV)
 			except:
-				error_list = [[data_file,'Cannot access .uv file']]
+				error_list = [[newUV,'Cannot access .uv file']]
 				for item in error_list:
 					ewr.writerow(item)
 				continue
 
-			print data_file
 			#find Julian Date
 			jdate = str(uv['time'])
 
@@ -81,4 +94,4 @@ for root, dirs, files in os.walk(data):
 			print newfile
 		
 			#copy data from one file to the other directory
-			shutil.copy(data_file,newfile)
+			shutil.move(newUV,newfile)
