3cc7f73457604bd8d00bc8e3de260ec40ab4779f
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-10-12 18:12:42 -0400

    Fixed to make lookup faster

--- paperdataDB/paperdataDB.py
+++ paperdataDB/paperdataDB.py
@@ -47,7 +47,7 @@ NONE = 24
 SEARCH = 30
 NOSEARCH = 31
 
-paperdata_dict = {PATH:'path', ERA:'era', ERA_TYPE:'era_type', OBSNUM:'obsnum', MD5SUM:'md5sum', JDAY:'julian_day', JDATE:'julian_date', POL:'polarization', LENGTH:'data_length', RAW_PATH:'raw_location', CAL_PATH:'cal_location', TAPE_PATH:'tape_location', COMPR_SIZE:'file_size_MB', RAW_SIZE:'raw_file_size_MB', COMPRESSED:'compressed', READY_TO_TAPE:'ready_to_tape', DELETE_FILE:'delete_file', RESTORE:'restore_history'}
+pd_dict = {PATH:'path', ERA:'era', ERA_TYPE:'era_type', OBSNUM:'obsnum', MD5SUM:'md5sum', JDAY:'julian_day', JDATE:'julian_date', POL:'polarization', LENGTH:'data_length', RAW_PATH:'raw_location', CAL_PATH:'cal_location', TAPE_PATH:'tape_location', COMPR_SIZE:'file_size_MB', RAW_SIZE:'raw_file_size_MB', COMPRESSED:'compressed', READY_TO_TAPE:'ready_to_tape', DELETE_FILE:'delete_file', RESTORE:'restore_history'}
 
 def fields():
 	field_list = ['path', 'era', 'era_type', 'obsnum', 'md5sum', 'julian_day', 'julian_date', 'polarization', 'data_length', 'raw_location', 'cal_location', 'tape_location', 'file_size_MB', 'raw_file_size_MB', 'compressed', 'ready_to_tape', 'delete_file', 'restore_history']
@@ -91,7 +91,7 @@ def fetch(info_list):
 	searchstr = []
 
 	#info list should be [[field_name, search_field, option, [more_info]], 
-	#ex: [['era', 'nosearch', 'exact', [32]], ['path', 'search', 'none', []], ['julian_day', ' search', 'range', [922, 935]]]
+	#ex: [['era', NOSEARCH, EXACT, [32]], ['path', SEARCH, NONE, []], ['julian_day', SEARCH, RANGE, [922, 935]]]
 	for item in info_list:
 		if len(item) != 4:
 			print 'ERROR -- LIST %s does not have enough entries' %(item)
@@ -100,7 +100,7 @@ def fetch(info_list):
 		#instantiates field variable
 		field = item[0]
 
-		if item[2] == 'exact':
+		if item[2] == EXACT:
 			if len(item[3]) != 1:
 				print 'ERROR -- LIST %s does not have the right amount of entries' %(item)
 				sys.exit() #HOW SHOULD I THROW ERRORS?
@@ -108,11 +108,11 @@ def fetch(info_list):
 			exact = item[3][0]
 
 			#adding info to lists to generate strings later
-			if item[1] == 'search':
+			if item[1] == SEARCH:
 				query.append(field)
 			searchstr.append('%s = %d'%(field, exact))
 
-		elif item[2] == 'min':
+		elif item[2] == MIN:
 			if len(item[3]) != 1:
 				print 'ERROR -- LIST %s does not have the right amount of entries' %(item)
 				sys.exit() #HOW SHOULD I THROW ERRORS?
@@ -120,11 +120,11 @@ def fetch(info_list):
 			min = item[3][0]
 
 			#adding info to lists to generate strings later
-			if item[1] == 'search':
+			if item[1] == SEARCH:
 				query.append(field)
 			searchstr.append('%s >= %d'%(field, min))
 
-		elif item[2] == 'max':
+		elif item[2] == MAX:
 			if len(item[3]) != 1:
 				print 'ERROR -- LIST %s does not have the right amount of entries' %(item) 
 				sys.exit() #HOW SHOULD I THROW ERRORS?
@@ -132,11 +132,11 @@ def fetch(info_list):
 			max = item[3][0]
 
 			#adding info to lists to generate strings later
-			if item[1] == 'search':
+			if item[1] == SEARCH:
 				query.append(field)
 			searchstr.append('%s <= %d'%(field, max))
 
-		elif item[2] == 'range':
+		elif item[2] == RANGE:
 			if len(item[3]) != 2:
 				print 'ERROR -- LIST %s does not have the right amount of entries' %(item) 
 				sys.exit() #HOW SHOULD I THROW ERRORS?
@@ -145,17 +145,17 @@ def fetch(info_list):
 			max = item[3][1]
 
 			#adding info to lists to generate strings later
-			if item[1] == 'search':
+			if item[1] == SEARCH:
 				query.append(field)
 			searchstr.append('%s >= %d and %s <= %d'%(field, min, field, max))
 
-		elif item[2] == 'none':
+		elif item[2] == NONE:
 			if len(item[3]) != 0:
 				print 'ERROR -- LIST %s has too many entries' %(item) 
 				sys.exit() #HOW SHOULD I THROW ERRORS?
 
 			#adding info to lists to generate strings later
-			if item[1] == 'search':
+			if item[1] == SEARCH:
 				query.append(field)
 
 		else:
