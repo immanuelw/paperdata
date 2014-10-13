77194ee6c2d594ae83d40a6bcafd3a896b1fb7f9
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-10-13 18:34:14 -0400

    Clears only paperdata

--- clear_paperdata.py
+++ clear_paperdata.py
@@ -7,18 +7,16 @@ import MySQLdb
 import sys
 import getpass
 
-datab = raw_input('Database:')
 pswd = getpass.getpass('Password:')
-qry = sys.argv[1]
 # open a database connection
 # be sure to change the host IP address, username, password and database name to match your own
-connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = datab, local_infile=True)
+connection = MySQLdb.connect (host = 'shredder', passwd = pswd, db = 'paperdata', local_infile=True)
 
 # prepare a cursor object using cursor() method
 cursor = connection.cursor()
 
 # execute the SQL query using execute() method.
-cursor.execute('USE paperdata; DELETE FROM %s' %(sys.argv[1]))
+cursor.execute('''TRUNCATE table paperdata''')
 print 'Table data deleted.' 
 
 # close the cursor object
