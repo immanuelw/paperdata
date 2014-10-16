d19f07fd78d494df609cfd0db74d4f36d62ae43e
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-10-15 22:53:44 -0400

    Added radiobutton functionality

--- search_paperdata.py
+++ search_paperdata.py
@@ -29,37 +29,6 @@ def makeform(root, fields):
 			row2 = Frame(root)
 			row2.pack(side=TOP, fill=X, padx=3, pady=3)
 
-#			ivar = IntVar()
-#			f = Checkbutton(row2, text='NONE', variable=ivar, onvalue = pdb.NONE, offvalue = 0)
-#			f.var = ivar
-#			f.pack(side=LEFT)
-
-#			ivar = IntVar()
-#			g = Checkbutton(row2, text='MIN', variable=ivar, onvalue = pdb.MIN, offvalue = 0)
-#			g.var = ivar
-#			g.pack(side=LEFT)
-
-#			ivar = IntVar()
-#			h = Checkbutton(row2, text='MAX', variable=ivar, onvalue = pdb.MAX, offvalue = 0)
-#			h.var = ivar
-#			h.pack(side=LEFT)
-
-#			ivar = IntVar()
-#			i = Checkbutton(row2, text='EXACT', variable=ivar, onvalue = pdb.EXACT, offvalue = 0)
-#			i.var = ivar
-#			i.pack(side=LEFT)
-
-#			ivar = IntVar()
-#			j = Checkbutton(row2, text='RANGE', variable=ivar, onvalue = pdb.RANGE, offvalue = 0)
-#			j.var = ivar
-#			j.pack(side=LEFT)
-
-#			for key, value in pdb.options().items():
-#				ivar = IntVar()
-#				a = Checkbutton(row2, text=value, variable=ivar, onvalue = key, offvalue = 0)
-#				a.var = ivar
-#				a.pack(side=LEFT)
-
 			ivar = IntVar()
 			ivar.set(pdb.NONE)
 			for key, value in pdb.options().items():
@@ -70,9 +39,7 @@ def makeform(root, fields):
 			lab.pack(side=LEFT)
 			c.pack(side=LEFT)
 			ent.pack(side=RIGHT, expand=YES, fill=X)
-			#print f.var
 			#creates entries list
-#			entries.append((field, c.var, f.var, g.var, h.var, i.var, j.var, ent))
 			entries.append((field, c.var, a.var, ent))
 	return entries
 
@@ -80,6 +47,7 @@ def convert(entries):
 	for entry in entries:
 		print entry[1].get()
 		print entry[2].get()
+		print entry[3].get()
 
 if __name__ == '__main__':
 	root = Tk()
