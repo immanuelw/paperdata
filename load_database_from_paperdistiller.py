58cc9c4faa5463e1c81906d4261e9c0d79e451f5
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-11-21 17:53:24 -0500

    Overhaul to fix all issues

--- load_database_from_paperdistiller.py
+++ load_database_from_paperdistiller.py
@@ -7,9 +7,9 @@ import MySQLdb
 import sys
 import getpass
 import os
-import inspect
 import csv
 import time
+import load_paperdata
 
 ### Script to load infromation quickly from paperdistiller database into paperdata
 ### Queries paperdistiller for relevant information, loads paperdata with complete info
@@ -35,30 +35,18 @@ def sizeof_fmt(num):
 	return "%3.1f" % (num)
 
 #User input information
-datab = 'paperdata'
 usrnm = raw_input('Username: ')
 pswd = getpass.getpass('Password: ')
 
-paperd = 'paperdistiller'
+auto_load = raw_input('Automatically load into paperdata? (y/n): ')
 
 time_date = time.strftime("%d-%m-%Y_%H:%M:%S")
-db128 = '/data2/home/immwa/scripts/paper_output/db_output128_%s.csv'%(time_date)
+dbnum = '/data4/paper/paperdistiller_output/paperdistiller_output_%s.csv'%(time_date)
+dbe = '/data4/paper/paperdistiller_output/paperdistiller_error_%s.csv'%(time_date)
 
 host = socket.gethostname()
 
-#searches for only particular files
-dbnum = db128
-
-resultFile = open(dbnum,'wb')
-
-#create 'writer' object
-wr = csv.writer(resultFile, dialect='excel')
-
-#empty list to use later
-results = []
-
 #pulls all relevant information from full paperdistiller database
-
 connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdistiller', local_infile=True)
 
 # prepare a cursor object using cursor() method
@@ -66,73 +54,106 @@ cursor = connection.cursor()
 
 # execute the SQL query using execute() method.
 cursor.execute('SELECT filename, obsnum, md5sum from file order by obsnum')
-
 result = cursor.fetchall()
 
 #gather all data to input into paperdata
+results = []
 for item in result:
 	cursor.execute('SELECT julian_date, pol, length from observation where obsnum = %d'%(item[1])
 	sec_results = cursor.fetchall()
-	result.append(item + sec_results)	
-
-#Close database connection and save changes
-cursor.close()
-connection.close()
+	results.append(item + sec_results[0])	
 
 #Create list of obsnums to check for duplicates
-connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdata', local_infile=True)
-
-cursor = connection.cursor()
-
 cursor.execute('SELECT obsnum from paperdata')
 obs = cursor.fetchall()
 
-#Close db connection and save changes
+#Close db connection
 cursor.close()
 connection.close()
 
 #results list of lists should contain path, obsnum, julian_date, polarization string, and length
 for item in results:
+	#Opens error logging file
+	error_file = open(dbe,'a')
+	ewr = csv.writer(error_file, dialect='excel')
 	#check for duplicate
 	if item[1] in obs:
+		err = [item, 'Obsnum already in paperdata']
+		ewr.writerow(err)
+		error_file.close()
 		continue
+
+	#Open file to write to
+	data_file = open(dbnum,'a')
+	#create 'writer' object
+	wr = csv.writer(data_file, dialect='excel')
 
 	#indicates location of raw file (usually same directory as compressed)	
 	###need to include host name in path_raw and path
-	path_raw = item[0]
 	
 	if item[0].split('.')[-1] == 'uv':
-		path = item[0] + 'cRRE'
-		compr_path = host + ':' + item[0] + 'cRRE'
+		raw_path = item[0]
+		raw_full_path = host + ':' + raw_path
+		compr_path = item[0] + 'cRRE'
+		compr_full_path = host + ':' + compr_path
 	elif item[0].split('.')[-1] == 'uv/':
-		path = item[0][:-1] + 'cRRE'
-		compr_path = host + ':' + item[0][:-1] + 'cRRE'
+		raw_path = item[0][:-1]
+		raw_full_path = host + ':' + raw_path
+		compr_path = item[0][:-1] + 'cRRE'
+		compr_full_path = host + ':' + compr_path
+	else:
+		err = [item, 'Not .uv file']
+		ewr.writerow(err)
+		error_file.close()
+		data_file.close()
+		continue
 
 	#indicates size of compressed file MB
-	sz = sizeof_fmt(get_size(path))
+	compr_file = os.path.join(compr_path, 'visdata')
+	if os.path.isfile(compr_file):
+		compr_sz = round(sizeof_fmt(get_size(compr_path)), 1)
+		compressed = 1
+	else:
+		compr_sz = 0.0
+		compressed = 0
+		err = [item, 'No compressed file']
+		ewr.writerow(err)
+		error_file.close()
+		continue
+		
 
 	#indicates size of raw file in MB
-	raw_sz = sizeof_fmt(get_size(path_raw))
-
-	#checks if any data is in file
-	visdata = os.path.join(path_raw, 'visdata')
-	if not os.path.isfile(visdata):
-		continue
+	raw_file = os.path.join(raw_path, 'visdata')
+	if os.path.isfile(raw_file):
+		raw_sz = sizeof_fmt(get_size(raw_path))
+	else:
+		err = [item, 'No .uv file']
+		ewr.writerow(err)
+		error_file.close()
 
         #allows uv access
 	try:
-	        uv = A.miriad.UV(path_raw)
+	        uv = A.miriad.UV(raw_path)
 	except:
+		err = [item, 'Cannot access .uv file']
+		ewr.writerow(err)
+		error_file.close()
+		data_file.close()
 		continue	
 
 	#indicates julian date
-	jdate = item[3]
+	jdate = round(float(item[3]), 5)
 
 	#indicates julian day
 	jday = int(str(jdate)[3:7])	
 
 	#indicates set of data used
-	era = 128
+	if jdate < 2456100:
+		era = 32
+	elif jdate < 2456400:
+		era = 64
+	else:
+		era = 128
 
 	#indicates type of file in era
 	era_type = 'NULL'
@@ -141,26 +162,16 @@ for item in results:
 	polarization = item[4]
 
 	#indicates length of information in file
-	length = item[5]
+	length = round(float(item[5]),5)
 
 	#indicates obsnum
-	obsnum = item[1]
+	obsnum = int(item[1])
 
 	#indicates md5sum
 	md5sum = item[2]
 
-	#location of raw files
-	raw_location = host + ':' + path_raw #do not know where they are for any of them yet
-
 	#location of calibrate files
 	cal_location = 'NULL'
-
-	#indicates if file is compressed
-	compr_file = os.path.join(path,'visdata')
-	if os.path.isfile(compr_file):
-		compressed = 1
-	else:
-		compressed = 0
 
 	#shows location of raw data on tape
 	tape_location = 'NULL'
@@ -178,31 +189,15 @@ for item in results:
 	restore_history = 'NULL'
 
 	#create list of important data and open csv file
-	databs = [compr_path,era,era_type,obsnum,md5sum,jday,jdate,polarization,length,raw_location,cal_location,tape_location,sz,raw_sz,compressed,edge,ready_to_tape,delete_file,restore_history]
+	databs = [compr_full_path,era,era_type,obsnum,md5sum,jday,jdate,polarization,length,raw_full_path,cal_location,tape_location,compr_sz,raw_sz,compressed,edge,ready_to_tape,delete_file,restore_history]
 	print databs 
 
 	#write to csv file by item in list
 	wr.writerow(databs)
+	data_file.close()
 
-#Load data into named database and table
-
-# open a database connection
-# be sure to change the host IP address, username, password and database name to match your own
-connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdata', local_infile=True)
-
-# prepare a cursor object using cursor() method
-cursor = connection.cursor()
-
-print dbnum 
-# execute the SQL query using execute() method.
-cursor.execute('''LOAD DATA LOCAL INFILE '%s' INTO TABLE paperdata COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' '''%(dbnum))
-
-print 'Table data loaded.'
-
-#Close db connection and save changes
-cursor.close()
-connection.commit()
-connection.close()
-
-# exit the program
-sys.exit()
+if auto_load == 'y':
+	#Load information into paperdata
+	load_paperdata.load_db(dbnum, usrnm, pswd)
+else:
+	print '''Information logged into '%s' ''' %(dbnum)
