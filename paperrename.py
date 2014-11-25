a8f8f00bfaa4fe4392a5edf5f57507fe75ca4cc1
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-11-24 21:18:12 -0500

    Added another email

--- paperrename.py
+++ paperrename.py
@@ -62,7 +62,8 @@ def email_paperrename(files)
 
 	server.sendmail('paperfeed.paperdata@gmail.com', 'immwa@sas.upenn.edu', msgs)
 	#server.sendmail('paperfeed.paperdata@gmail.com', 'jaguirre@sas.upenn.edu', msgs)
-
+	#server.sendmail('paperfeed.paperdata@gmail.com', 'saul.aryeh.kohn@gmail.com', msgs)
+	
 	return None
 
 def check_data(usrnm, pswd):
