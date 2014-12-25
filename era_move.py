ddd305c474d1f5b503a5971df9315d4cf6fb8ee0
Immanuel Washington <immwa@folio.sas.upenn.edu>
2014-12-24 20:34:56 -0500

    Added way to batch move raw files

--- era_move.py
+++ era_move.py
@@ -29,32 +29,40 @@ if __name__ == '__main__':
 	usrnm = raw_input('Username: ')
 	pswd = getpass.getpass('Password: ')
 
+	#checks for input file type
+	file_type = raw_input('uv or uvcRRE?: ')
+	#file_type = 'uvcRRE'
+
+	if not file_type in ['uv','uvcRRE']:
+		print 'Invalid file type'
+		sys.exit()
+
 	#File information
-	connection = MySQLdb.connect (host = 'shredder', user = usrnm, passwd = pswd, db = 'paperdata', local_infile=True)
+	connection = MySQLdb.connect (host = 'shredder', user = 'paperboy', passwd = 'paperboy', db = 'paperdata', local_infile=True)
 	cursor = connection.cursor()
-	cursor.execute('''SELECT SUBSTRING_INDEX(path, 'z', 1), julian_day from paperdata where path like '%/raw_data/psa%' group by SUBSTRING_INDEX(path, 'z', 1) order by julian_day asc''')
+
+	if file_type == 'uvcRRE':
+		cursor.execute('''SELECT SUBSTRING_INDEX(path, 'z', 1), julian_day from paperdata where path like '%/raw_data/psa%' group by SUBSTRING_INDEX(path, 'z', 1) order by julian_day asc''')
+	elif file_type == 'uv':
+		cursor.execute('''SELECT SUBSTRING_INDEX(raw_path, 'z', 1), julian_day from paperdata where raw_path like '%/raw_data/psa%' group by SUBSTRING_INDEX(raw_path, 'z', 1) order by julian_day asc''')
 	jdays = cursor.fetchall()
 	cursor.close()
 	connection.close()
 
 	for jday in jdays:
-
-		infile = os.path.join(jday[0].split(':')[1], 'zen.*.uvcRRE')
-		outfile = os.path.join('/data4/paper/2011EoR/', '245' + str(jday[1]))
-		print infile, outfile
+		if file_type == 'uvcRRE':
+			infile = os.path.join(jday[0].split(':')[1], 'zen.*.uvcRRE')
+			outfile = os.path.join('/data4/paper/2011EoR/', '245' + str(jday[1]))
+			print infile, outfile
+		elif file_type == 'uv':
+			infile = os.path.join(jday[0].split(':')[1], 'zen.*.uv')
+			outfile = os.path.join('/data4/paper/raw_to_tape/', '245' + str(jday[1]))
+			print infile, outfile
 
 		#checks to see that output dir is valid
 		if not os.path.isdir(outfile):
 			print 'Output directory does not exist'
 			os.mkdir(outfile)
-
-		#checks for input file type
-		#file_type = raw_input('uv or uvcRRE?: ')
-		file_type = 'uvcRRE'
-
-		if not file_type in ['uv','uvcRRE']:
-			print 'Invalid file type'
-			sys.exit()
 
 		#List of files in directory -- allowing mass movement of .uvcRRE and .uv files
 		infile_list = glob.glob(infile)
