71b56683fc77769cdfbc44648f15df18f3205a1a
Immanuel Washington <immwa@folio.sas.upenn.edu>
2015-01-26 00:54:17 -0500

    Restructure

---
+++ paperdataDB/setup.py
@@ -0,0 +1,18 @@
+import glob
+
+__version__ = '0.0.1'
+
+setup_args = {
+	'name': 'paperdataDB',
+	'author': 'Immanuel Washington',
+	'author_email': 'immwa at sas.upenn.edu',
+	'license': 'GPL',
+	'package_dir' : {'paperdataDB':'src'},
+	'packages' : ['paperdataDB'],
+	'scripts': glob.glob('scripts/*'),
+	'version': __version__,
+}
+
+if __name__ == '__main__':
+	from distutils.core import setup
+	apply(setup, (), setup_args)
