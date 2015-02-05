373935f3a0e6b0491233c5c61daa1e6ee9e30eaf
Immanuel Washington <immwa@folio.sas.upenn.edu>
2015-02-04 23:47:45 -0500

    Fixed classes

--- pyganglia/__init__.py
+++ pyganglia/__init__.py
@@ -84,71 +84,75 @@ def dbsearch(query):
 
 	return results
 
-class monitor_files(self):
-	self.FILENAME = 11
-	self.STATUS = 12
-	self.DEL_TIME = 13
-	self.STILL_HOST = 14
-	self.TIME_DATE = 15
-	self.db_list = ['filename', 'status', 'del_time', 'still_host', 'time_date']
-	self.db_dict = {self.FILENAME:'filename', self.STATUS:'status', self.DEL_TIME'del_time', self.STILL_HOST'still_host', self.TIME_DATE:'time_date'}
-	self.var_flo = []
-	self.var_str = ['filename', 'status', 'still_host', 'time_date']
-	self.var_int = ['del_time']
-	self.table = 'monitor_files'
+class monitor_files:
+	def __init__(self):
+		self.FILENAME = 11
+		self.STATUS = 12
+		self.DEL_TIME = 13
+		self.STILL_HOST = 14
+		self.TIME_DATE = 15
+		self.db_list = ['filename', 'status', 'del_time', 'still_host', 'time_date']
+		self.db_dict = {self.FILENAME:'filename', self.STATUS:'status', self.DEL_TIME:'del_time', self.STILL_HOST:'still_host', self.TIME_DATE:'time_date'}
+		self.var_flo = []
+		self.var_str = ['filename', 'status', 'still_host', 'time_date']
+		self.var_int = ['del_time']
+		self.table = 'monitor_files'
 
-class ram(self):
-	self.HOST = 11
-	self.TOTAL = 12
-	self.USED = 13
-	self.FREE = 14
-	self.SHARED = 15
-	self.BUFFERS = 16
-	self.CACHED = 17
-	self.BC_USED = 18
-	self.BC_FREE = 19
-	self.SWAP_TOTAL = 20
-	self.SWAP_USED = 21
-	self.SWAP_FREE = 22
-	self.TIME_DATE = 23
-	self.db_list = ['host', 'total', 'used', 'free', 'shared', 'buffers', 'cached', 'bc_used', 'bc_free', 'swap_total', 'swap_used', 'swap_free', 'time_date']
-	self.db_dict = {self.HOST:'host', self.TOTAL:'total', self.USED:'used', self.FREE:'free', self.SHARED:'shared', self.BUFFERS:'buffers', self.CACHED:'cached', self.BC_USED:'bc_used', self.BC_FREE:'bc_free', self.SWAP_TOTAL:'swap_total', self.SWAP_TOTAL:'swap_used', self.SWAP_FREE:'swap_free', self.TIME_DATE:'time_date'}
-	self.var_flo = []
-	self.var_str = ['host', 'time_date']
-	self.var_int = ['total', 'used', 'free', 'shared', 'buffers', 'cached', 'bc_used', 'bc_free', 'swap_total', 'swap_used', 'swap_free']
-	self.table = 'ram'
+class ram:
+	def __init__(self):
+		self.HOST = 11
+		self.TOTAL = 12
+		self.USED = 13
+		self.FREE = 14
+		self.SHARED = 15
+		self.BUFFERS = 16
+		self.CACHED = 17
+		self.BC_USED = 18
+		self.BC_FREE = 19
+		self.SWAP_TOTAL = 20
+		self.SWAP_USED = 21
+		self.SWAP_FREE = 22
+		self.TIME_DATE = 23
+		self.db_list = ['host', 'total', 'used', 'free', 'shared', 'buffers', 'cached', 'bc_used', 'bc_free', 'swap_total', 'swap_used', 'swap_free', 'time_date']
+		self.db_dict = {self.HOST:'host', self.TOTAL:'total', self.USED:'used', self.FREE:'free', self.SHARED:'shared', self.BUFFERS:'buffers', self.CACHED:'cached', self.BC_USED:'bc_used', self.BC_FREE:'bc_free', self.SWAP_TOTAL:'swap_total', self.SWAP_TOTAL:'swap_used', self.SWAP_FREE:'swap_free', self.TIME_DATE:'time_date'}
+		self.var_flo = []
+		self.var_str = ['host', 'time_date']
+		self.var_int = ['total', 'used', 'free', 'shared', 'buffers', 'cached', 'bc_used', 'bc_free', 'swap_total', 'swap_used', 'swap_free']
+		self.table = 'ram'
 
-class iostat(self):
-	self.HOST = 11
-	self.DEVICE = 12
-	self.TPS = 13
-	self.READ_S = 14
-	self.WRITE_S = 15
-	self.READS = 16
-	self.WRITES = 17
-	self.TIME_DATE = 18
-	self.db_list = ['host', 'device', 'tps', 'read_s', 'write_s', 'reads', 'writes', 'time_date']
-	self.db_dict = {self.HOST:'host', self.DEVICE:'device', self.TPS:'tps', self.READ_S:'read_s', self.WRITE_S:'write_s', self.READS:'reads', self.WRITES:'writes', self.TIME_DATE:'time_date'}
-	self.var_flo = ['tps', 'read_s', 'write_s']
-	self.var_str = ['host', 'device', 'time_date']
-	self.var_int = ['reads', 'writes']
-	self.table = 'iostat'
+class iostat:
+	def __init__(self):
+		self.HOST = 11
+		self.DEVICE = 12
+		self.TPS = 13
+		self.READ_S = 14
+		self.WRITE_S = 15
+		self.READS = 16
+		self.WRITES = 17
+		self.TIME_DATE = 18
+		self.db_list = ['host', 'device', 'tps', 'read_s', 'write_s', 'reads', 'writes', 'time_date']
+		self.db_dict = {self.HOST:'host', self.DEVICE:'device', self.TPS:'tps', self.READ_S:'read_s', self.WRITE_S:'write_s', self.READS:'reads', self.WRITES:'writes', self.TIME_DATE:'time_date'}
+		self.var_flo = ['tps', 'read_s', 'write_s']
+		self.var_str = ['host', 'device', 'time_date']
+		self.var_int = ['reads', 'writes']
+		self.table = 'iostat'
 
-class cpu(self):
-	self.HOST = 11
-	self.CPU = 12
-	self.USER_PERC = 13
-	self.SYS_PERC = 14
-	self.IOWAIT_PERC = 15
-	self.IDLE_PERC = 16
-	self.INTR_S = 17
-	self.TIME_DATE = 18
-	self.db_list = ['host','cpu', 'user_perc', 'sys_perc', 'iowait_perc', 'idle_perc', 'intr_s', 'time_date']
-	self.db_dict = {self.HOST:'host', self.CPU:'cpu', self.USER_PERC:'user_perc', self.SYS_PERC:'sys_perc', self.IOWAIT_PERC:'iowait_perc', self.IDLE_PERC:'idle_perc', self.INTR_S:'intr_s', self.TIME_DATE:'time_date'}
-	self.var_flo = ['user_perc', 'sys_perc', 'iowait_perc', 'idle_perc']
-	self.var_str = ['host', 'time_date']
-	self.var_int = ['cpu', 'intr_s']
-	self.table = 'cpu'
+class cpu:
+	def __init__(self):
+		self.HOST = 11
+		self.CPU = 12
+		self.USER_PERC = 13
+		self.SYS_PERC = 14
+		self.IOWAIT_PERC = 15
+		self.IDLE_PERC = 16
+		self.INTR_S = 17
+		self.TIME_DATE = 18
+		self.db_list = ['host','cpu', 'user_perc', 'sys_perc', 'iowait_perc', 'idle_perc', 'intr_s', 'time_date']
+		self.db_dict = {self.HOST:'host', self.CPU:'cpu', self.USER_PERC:'user_perc', self.SYS_PERC:'sys_perc', self.IOWAIT_PERC:'iowait_perc', self.IDLE_PERC:'idle_perc', self.INTR_S:'intr_s', self.TIME_DATE:'time_date'}
+		self.var_flo = ['user_perc', 'sys_perc', 'iowait_perc', 'idle_perc']
+		self.var_str = ['host', 'time_date']
+		self.var_int = ['cpu', 'intr_s']
+		self.table = 'cpu'
 
 # Generate strings to load into query 
 def fetch(info_list, db_dict, var_flo, var_str, var_int, table):
@@ -165,7 +169,7 @@ def fetch(info_list, db_dict, var_flo, var_str, var_int, table):
 
 		# Instantiates field variable
 		if isinstance(item[0], str):
-		field = item[0]
+			field = item[0]
 		elif isinstance(item[0], int):
 			field = db_dict[item[0]]
 
@@ -317,7 +321,6 @@ class searchobj:
 		self.var_str = var_str
 		self.var_int = var_int
 		self.table = table
-		return None
 	def add_to_output(self, field, search_bool, limit, *range):
 		self.info_list.append([field, search_bool, limit, range])
 		return None
