77fef61ad4f98aad38948e95f172c2b69b260be8
Immanuel Washington <immwa@folio.sas.upenn.edu>
2015-02-20 01:22:06 -0500

    Should work to launch all daemons simultanouesly

--- pyganglia/scripts/launch_logs.sh
+++ pyganglia/scripts/launch_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for i in folio node01 node02 node03 node04 node05 node06 node07 node08 node09 node10; do ssh $i && source /usr/global/paper/CanopyVirtualEnvs/PAPER_Distiller/bin/activate && python /data2/home/immwa/scripts/paper/pyganglia/scripts/node_log.py y & done
+for i in folio node01 node02 node03 node04 node05 node06 node07 node08 node09 node10; do ssh -T $i  '/usr/global/paper/CanopyVirtualEnvs/PAPER_Distiller/bin/activate; python /data2/home/immwa/scripts/paper/pyganglia/scripts/node_log.py y &;' done
