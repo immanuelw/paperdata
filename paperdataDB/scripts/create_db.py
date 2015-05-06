b7aae0f505d42e3c4a54c3bc9ad80e4a1162962f
yatsu <immwa@sas.upenn.edu>
2015-05-06 03:30:48 -0400

    Instantiates paperdata dbi tables

---
+++ paperdataDB/scripts/create_db.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Create paperdata tables
+
+import paperdata_dbi
+
+
+### Script to create paperdata database
+### Instantiates tables
+
+### Author: Immanuel Washington
+### Date: 5-06-15
+
+def create_db():
+	dbi = paperdata_dbi.DatabaseInterface()
+	dbi.create_db()
+
+if __name__ == '__main__':
+	create_db()
