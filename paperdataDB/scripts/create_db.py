68d00b8eb3663ede9bf766b62620d3301d1f141d
yatsu <immwa@sas.upenn.edu>
2015-05-06 03:46:59 -0400

    Typo

--- paperdataDB/scripts/create_db.py
+++ paperdataDB/scripts/create_db.py
@@ -12,7 +12,7 @@ import paperdata_dbi
 ### Date: 5-06-15
 
 def create_db():
-	dbi = paperdata_dbi.DatabaseInterface()
+	dbi = paperdata_dbi.DataBaseInterface()
 	dbi.create_db()
 
 if __name__ == '__main__':
