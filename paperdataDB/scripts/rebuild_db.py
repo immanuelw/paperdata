112ef1daaf6af4788937a9238397376c5b33f752
yatsu <immwa@sas.upenn.edu>
2015-05-06 09:56:30 -0400

    Should rebuild db

--- paperdataDB/scripts/rebuild_db.py
+++ paperdataDB/scripts/rebuild_db.py
@@ -93,6 +93,7 @@ def get_prev_obs(obsnum):
 		prev_obs = 'NULL' #XXXX --- should it be 0 or maybe empty?
 	else:
 		prev_obs = int(results[0][0])
+
 	return prev_obs
 
 def get_next_obs(obsnum):
@@ -108,26 +109,78 @@ def get_next_obs(obsnum):
 		next_obs = 'NULL' #XXXX --- should it be 0 or maybe empty?
 	else:
 		next_obs = int(results[0][0])
+
 	return next_obs
 
-def idk(host, path, filename):
+def calc_times(host, path, filename):
 	named_host = socket.gethostname()
 	full_path = os.path.join(path, filename)
+	times =  ('NULL', 'NULL', 'NULL')
 	if named_host == host:
-		#do stuff
+		#allows uv access
+		try:
+			uv = A.miriad.UV(full_path)
+		except:
+			return times
+
+		time_start = 0
+		time_end = 0
+		n_times = 0
+		c_time = 0
+
+		try:
+			for (uvw, t, (i,j)),d in uv.all():
+				if time_start == 0 or t < time_start:
+					time_start = t
+				if time_end == 0 or t > time_end:
+					time_end = t
+				if c_time != t:
+					c_time = t
+					n_times += 1
+		except:
+			return times
+
+		if n_times > 1:
+			delta_time = -(time_start - time_end)/(n_times - 1)
+		else:
+			delta_time = -(time_start - time_end)/(n_times)
+
+		times = (time_start, time_end, delta_time)
 	#else:
 		##SSH INTO CORRECT HOST -- PARAMIKO??
-			#also do stuff
+			#allows uv access
+			#try:
+			#	uv = A.miriad.UV(full_path)
+			#except:
+			#	return times
+
+			#time_start = 0
+			#time_end = 0
+			#n_times = 0
+			#c_time = 0
+
+			#try:
+			#	for (uvw, t, (i,j)),d in uv.all():
+			#		if time_start == 0 or t < time_start:
+			#			#time_start = t
+			#		if time_end == 0 or t > time_end:
+			#			#time_end = t
+			#		if c_time != t:
+			#			#c_time = t
+			#			#n_times += 1
+			#except:
+			#	return times
+
+			#if n_times > 1:
+			#	delta_time = -(time_start - time_end)/(n_times - 1)
+			#else:
+			#	delta_time = -(time_start - time_end)/(n_times)
+
+			#times = (time_start, time_end, delta_time)
+	
 		###EXIT??
 
-	#allows uv access
-	try:
-		uv = A.miriad.UV(path)
-	except:
-		item = [path,'Cannot access .uv file']
-		ewr.writerow(item)
-		error_file.close()
-		continue	
+	return times
 
 ### Backup Functions
 
@@ -171,58 +224,102 @@ def backup_files(dbnum2, dbnum3, dbnum4, dbnum5, time_date):
 			#host, path, filename, filetype, obsnum, filesize, md5sum, tape_index
 			cursor.execute('''SELECT SUBSTRING_INDEX(raw_path, ':', 1), SUBSTRING_INDEX(SUBSTRING_INDEX(raw_path, ':', -1), '/', -1), SUBSTRING_INDEX(SUBSTRING_INDEX(raw_path, ':', -1), '/z', 1), SUBSTRING_INDEX(raw_path, '.', -1), obsnum, raw_file_size_MB, md5sum, tape_index FROM paperdata where raw_path != 'NULL' group by raw_path order by julian_date asc, polarization asc''')
 			res = cursor.fetchall()
-			#need time_start, time_end, delta_time, prev_obs, next_obs functions
 			res1 = res
+			#need time_start, time_end, delta_time, prev_obs, next_obs functions
+			res2 = []
+			for item in res:
+				host = item[0]
+				path = item[1]
+				filename = item[2]
+				obsnum = item[4]
+
+				res2.append(calc_times(host, path, filename) + (get_prev_obs(obsnum), get_next_obs(obsnum)))
+			#convert back to tuple
+			res2 = tuple(res2)
+			#
 			#edge, write_to_tape, delete_file
 			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where raw_path != 'NULL' group by raw_path order by julian_date asc, polarization asc''')
-			res2 = cursor.fetchall()
-			#XXXX zip time_start tuple as well zip(res, res1, results)
-			resu = zip(res1, res2)
+			res3 = cursor.fetchall()
+			#XXXX zip time_start tuple as well zip(res1, res2, res3)
+			resu = zip(res1, res2, res3)
 			for item in resu:
 				if len(item) >= 2 and type(item[0]) is tuple:
 					results += tuple(i for i in item)
 
 		elif dbnum == dbnum3:
-			#host, path, filename, filetype, obsnum, filesize, md5sum, tape_index
+			#host, path, filename, filetype, obsnum, filesize
 			cursor.execute('''SELECT SUBSTRING_INDEX(path, ':', 1), SUBSTRING_INDEX(SUBSTRING_INDEX(path, ':', -1), '/', -1), SUBSTRING_INDEX(SUBSTRING_INDEX(path, ':', -1), '/z', 1), SUBSTRING_INDEX(path, '.', -1), obsnum, compr_file_size_MB FROM paperdata where path != 'NULL' group by path order by julian_date asc, polarization asc''')
 			res = cursor.fetchall()
-			#need md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
 			res1 = res
+			#need md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
+			res2 = []
+			for item in res:
+				host = item[0]
+				path = item[1]
+				filename = item[2]
+				obsnum = item[4]
+
+				res2.append((calc_md5sum(host, path, filename), 'NULL') + calc_times(host, path, filename) + (get_prev_obs(obsnum), get_next_obs(obsnum)))
+			#convert back to tuple
+			res2 = tuple(res2)
+			#
 			#edge, write_to_tape, delete_file
 			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where path != 'NULL' group by path order by julian_date asc, polarization asc''')
-			res2 = cursor.fetchall()
-			#XXXX zip time_start tuple as well zip(res, res1, results)
-			resu = zip(res1, res2)
+			res3 = cursor.fetchall()
+			#XXXX zip time_start tuple as well zip(res1, res2, res3)
+			resu = zip(res1, res2, res3)
 			for item in resu:
 				if len(item) >= 2 and type(item[0]) is tuple:
 					results += tuple(i for i in item)
 
 		elif dbnum == dbnum4:
-			#host, npz_path, filename, filetype, obsnum, filesize, md5sum, tape_index
+			#host, npz_path, filename, filetype, obsnum
 			cursor.execute('''SELECT SUBSTRING_INDEX(npz_path, ':', 1), SUBSTRING_INDEX(SUBSTRING_INDEX(npz_path, ':', -1), '/', -1), SUBSTRING_INDEX(SUBSTRING_INDEX(npz_path, ':', -1), '/z', 1), SUBSTRING_INDEX(npz_path, '.', -1), obsnum FROM paperdata where npz_path != 'NULL' group by npz_path order by julian_date asc, polarization asc''')
 			res = cursor.fetchall()
-			#need filesize, md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
 			res1 = res
+			#need filesize, md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
+			res2 = []
+			for item in res:
+				host = item[0]
+				path = item[1]
+				filename = item[2]
+				obsnum = item[4]
+
+				res2.append((calc_size(host, name, filename), calc_md5sum(host, path, filename), 'NULL') + calc_times(host, path, filename) + (get_prev_obs(obsnum), get_next_obs(obsnum)))
+			#convert back to tuple
+			res2 = tuple(res2)
+			#
 			#edge, write_to_tape, delete_file
-			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where npz_path != 'NULL' group by raw_npz_path order by julian_date asc, polarization asc''')
-			res2 = cursor.fetchall()
-			#XXXX zip time_start tuple as well zip(res, res1, results)
-			resu = zip(res1, res2)
+			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where npz_path != 'NULL' group by npz_path order by julian_date asc, polarization asc''')
+			res3 = cursor.fetchall()
+			#XXXX zip time_start tuple as well zip(res1, res2, res3)
+			resu = zip(res1, res2, res3)
 			for item in resu:
 				if len(item) >= 2 and type(item[0]) is tuple:
 					results += tuple(i for i in item)
 
 		elif dbnum == dbnum5:
-			#host, final_product_path, filename, filetype, obsnum, filesize, md5sum, tape_index
+			#host, final_product_path, filename, filetype, obsnum
 			cursor.execute('''SELECT SUBSTRING_INDEX(final_product_path, ':', 1), SUBSTRING_INDEX(SUBSTRING_INDEX(final_product_path, ':', -1), '/', -1), SUBSTRING_INDEX(SUBSTRING_INDEX(final_product_path, ':', -1), '/z', 1), SUBSTRING_INDEX(final_product_path, '.', -1), obsnum FROM paperdata where final_product_path != 'NULL' group by final_product_path order by julian_date asc, polarization asc''')
 			res = cursor.fetchall()
-			#need filesize, md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
 			res1 = res
+			#need filesize, md5sum, tape_index, time_start, time_end, delta_time, prev_obs, next_obs functions
+			res2 = []
+			for item in res:
+				host = item[0]
+				path = item[1]
+				filename = item[2]
+				obsnum = item[4]
+
+				res2.append((calc_size(host, name, filename), calc_md5sum(host, path, filename), 'NULL') + calc_times(host, path, filename) + (get_prev_obs(obsnum), get_next_obs(obsnum)))
+			#convert back to tuple
+			res2 = tuple(res2)
+			#
 			#edge, write_to_tape, delete_file
-			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where final_product_path != 'NULL' group by raw_final_product_path order by julian_date asc, polarization asc''')
-			res2 = cursor.fetchall()
-			#XXXX zip time_start tuple as well zip(res, res1, results)
-			resu = zip(res1, res2)
+			cursor.execute('''SELECT edge, write_to_tape, delete_file FROM paperdata where final_product_path != 'NULL' group by final_product_path order by julian_date asc, polarization asc''')
+			res3 = cursor.fetchall()
+			#XXXX zip time_start tuple as well zip(res1, res2, res3)
+			resu = zip(res1, res2, res3)
 			for item in resu:
 				if len(item) >= 2 and type(item[0]) is tuple:
 					results += tuple(i for i in item)
