eb19f0691a1cf98f04ccc47808bedeaf911978f2
yatsu <immwa@sas.upenn.edu>
2015-05-09 02:20:39 -0400

    Tested ssh successfully ;

--- paperdataDB/scripts/add_files.py
+++ paperdataDB/scripts/add_files.py
@@ -29,10 +29,10 @@ def login_ssh(host, username=None):
 	ssh.load_system_host_keys()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	try:
-		ssh.connect(host, username=username, key_filename='~/.ssh/id_rsa')
+		ssh.connect(host, username=username, key_filename='/home/{0}/.ssh/id_rsa'.format(username))
 	except:
 		try:
-			ssh.connect(host, key_filename='~/.ssh/id_rsa')
+			ssh.connect(host, key_filename='/home/{0}/.ssh/id_rsa'.format(username))
 		except:
 			return None
 
