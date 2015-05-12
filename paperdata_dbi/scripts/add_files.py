6f19dc1beee4f0d9ca6430939b5436319f6214fd
yatsu <immwa@sas.upenn.edu>
2015-05-12 18:22:18 -0400

    Removed unnecessary login ssh

--- paperdata_dbi/scripts/add_files.py
+++ paperdata_dbi/scripts/add_files.py
@@ -22,22 +22,6 @@ import paperdata_dbi
 ### Author: Immanuel Washington
 ### Date: 5-06-15
 
-#SSH/SFTP Function
-#Need private key so don't need username/password
-def paperdata_dbi.login_ssh(host, username=None):
-	ssh = paramiko.SSHClient()
-	ssh.load_system_host_keys()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	try:
-		ssh.connect(host, username=username, key_filename='/home/{0}/.ssh/id_rsa'.format(username))
-	except:
-		try:
-			ssh.connect(host, key_filename='/home/{0}/.ssh/id_rsa'.format(username))
-		except:
-			return None
-
-	return ssh
-
 #Functions which simply find the file size
 def get_size(start_path):
 	total_size = 0
