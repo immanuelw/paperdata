62bbfcd32879c21c7c32781bd766307bfef14aca
yatsu <immwa@sas.upenn.edu>
2015-05-17 21:44:24 -0400

    Added db schema

---
+++ paperdata_dbi/scripts/paperdata_db.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Module to creation of db schema
+
+### Author: Immanuel Washington
+### Date: 05-17-14
+
+classes = ('File', 'Observation')
+
+class File:
+	def __init__(self):
+		self.table = 'File'
+		self.db_list = ('host',
+						'path',
+						'filename',
+						'filetype',
+						'full_path',
+						'obsnum',
+						'filesize',
+						'md5sum',
+						'tape_index',
+						'write_to_tape',
+						'delete_file')
+		self.db_descr = {'host':('VARCHAR(100)', 'None', 'No', 'host of file system that file is located on'),
+						'path':('VARCHAR(100)', 'None', 'No', 'directory that file is located in'),
+						'filename':('VARCHAR(100)', 'None', 'No', 'name of file (ex: zen.2446321.16617.uvcRRE)'),
+						'filetype':('VARCHAR(20)', 'None', 'No', 'filetype (ex: uv, uvcRRE, npz)'),
+						'full_path':('VARCHAR(200)', 'None', 'Unique', 'combination of host, path, and filename which is a unique
+										identifier for each file'),
+						'obsnum':('BIGINT', 'None', 'Primary', 'observation number used to track files using integer'),
+						'filesize':('DECIMAL(7, 2)', 'None', 'No', 'size of file in megabytes'),
+						'md5sum':('INTEGER', 'None', 'No', '32-bit integer md5 checksum of file'),
+						'tape_index':('VARCHAR(100)', 'None', 'No', 'indexed location of file on tape'),
+						'write_to_tape':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file needs to be written to tape'),
+						'delete_file':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file needs to be deleted from its host')}
+
+class Observation:
+	def __init__(self):
+		self.table = 'Observation'
+		self.db_list = ('obsnum',
+						'julian_date',
+						'polarization',
+						'julian_day',
+						'era',
+						'era_type',
+						'length',
+						'time_start',
+						'time_end',
+						'delta_time',
+						'prev_obs',
+						'next_obs',
+						'edge')
+		self.db_descr = {'obsnum':('BIGINT', 'None', 'Primary', 'observation number used to track files using integer'),
+						'julian_date':('DECIMAL(12, 5)', 'None', 'No', 'julian date of observation'),
+						'polarization':('VARCHAR(4)', 'None', 'No', 'polarization of observation'),
+						'julian_day':('INTEGER', 'None', 'No',
+										'integer value -- the last 4 digits for any julian date to separate into days: ex:(2456601 -> 6601)'),
+						'era':('INTEGER', 'None', 'No', 'era of observation taken: 32, 64, 128'),
+						'era_type':('VARCHAR(20)'. 'None', 'No', 'type of observation taken: dual pol, etc.'),
+						'length':('DECIMAL(6, 5)', 'None'. 'No', 'length of time data was taken for particular observation'),
+						'time_start':('DECIMAL(12, 5)', 'None', 'No', 'start time of observation'),
+						'time_end':('DECIMAL(12, 5)', 'None', 'No', 'end time of observation'),
+						'delta_time':('DECIMAL(12, 5)', 'None', 'No', 'time step of observation'),
+						'prev_obs':('BIGINT', 'None', 'Unique', 'observation number of previous observation'),
+						'next_obs':('BIGINT', 'None', 'Unique', 'observation number of next observation'),
+						'edge':('BOOLEAN', 'None', 'No', 'boolean value indicating if observation at beginning/end of night or not')}
+
+#dictionary of instantiated classes
+instant_class = {'File':File(), 'Observation':Observation()}
+all_classes = (File(), Observation())
+
+#Only do things if running this script, not importing
+if __name__ == '__main__':
+	print 'Not a script file, just a module'
---
+++ paperdata_dbi/scripts/schema_db.py
@@ -0,0 +1,26 @@
+import sys
+import prettytable
+import paperdata_db as pdb
+
+def main():
+	with open('./schema.txt', 'wb') as df:
+		print 'Starting ..'
+	var_classes = pdb.all_classes
+
+	for var_class in var_classes:
+		x = prettytable.PrettyTable(['Field', 'Type', 'Default', 'Unique Key', 'Description'])
+		with open('../src/schema.txt', 'ab') as df:
+			df.write(var_class.table + '\n')
+		for field in var_class.db_list:
+			full_item = [field, var_class.db_descr[field][0], var_class.db_descr[field][1], var_class.db_descr[field][2], var_class.db_descr[field][3]]
+			x.add_row(full_item)
+			stuff = x.get_string()
+		with open('../src/schema.txt', 'ab') as df:
+			df.write(stuff)
+			df.write('\n')
+
+	print 'Done!'
+
+if __name__ == "__main__":
+	main()
+
