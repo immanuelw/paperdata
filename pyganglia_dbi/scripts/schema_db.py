2a0c1c082acf25dafe1b0176a4410906f28cd9e4
yatsu <immwa@sas.upenn.edu>
2015-05-27 15:37:47 -0400

    Fixed to pyg

--- pyganglia_dbi/scripts/schema_db.py
+++ pyganglia_dbi/scripts/schema_db.py
@@ -1,11 +1,11 @@
 import sys
 import prettytable
-import ganglia_db as gdb
+import pyganglia_db as pyg
 
 def main():
 	with open('./schema.txt', 'wb') as df:
 		print 'Starting ..'
-	var_classes = gdb.all_classes
+	var_classes = pyg.all_classes
 
 	for var_class in var_classes:
 		x = prettytable.PrettyTable(['Field', 'Type', 'Default', 'Unique Key', 'Description'])
