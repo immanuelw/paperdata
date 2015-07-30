446e1c0a0fb57c11bbbf14ab2a63a179fbd2e36d
Immanuel Washington <immwa@folio.sas.upenn.edu>
2015-07-30 12:27:26 -0400

    DRY

--- paperdata_dbi/scripts/load_backup_db.py
+++ paperdata_dbi/scripts/load_backup_db.py
@@ -14,59 +14,41 @@ import paperdata_db as pdb
 ### Author: Immanuel Washington
 ### Date: 5-06-15
 
-def load_backup_obs(backup_observation):
+def load_backup(backup, table=None):
 	dbi = pdbi.DataBaseInterface()
-	with open(backup_observation, 'r') as backup_obs:
-		read = json.load(backup_obs)
-		OBS_class = pdb.Observation()
-		obs_list = OBS_class.db_list
-		for row in read:
-			print tuple(row)
-			dbi.add_observation(**row)
+	with open(backup, 'r') as backup_db:
+		read = json.load(backup_db)
+		if table is None:
+			return None
+		elif table == 'observation':
+			OBS_class = pdb.Observation()
+			obs_list = OBS_class.db_list
+			for row in read:
+				print tuple(row)
+				try:
+					dbi.add_observation(**row)
+				except:
+					print('Failed to load in entry')
+		elif table == 'file':
+			FILE_class = pdb.File()
+			file_list = FILE_class.db_list
+			for row in read:
+				print tuple(row)
+				try:
+					dbi.add_file(**row)
+				except:
+					print('Failed to load in entry')
+		#elif table == 'feed':
+		#	FEED_class = pdb.Feed()
+		#	feed_list = FEED_class.db_list
+		#	for row in read:
+		#		print tuple(row)
+		#		try:
+		#			dbi.add_file(**row)
+		#		except:
+		#			print('Failed to load in entry')
+
 	return None
-
-def load_backup_files(backup_fi):
-	dbi = pdbi.DataBaseInterface()
-	with open(backup_fi, 'r') as backup_file:
-		read = json.load(backup_file)
-		FILE_class = pdb.File()
-		file_list = FILE_class.db_list
-		for row in read:
-			print tuple(row)
-			dbi.add_file(**row)
-	return None
-
-def check_row(row, s):
-	full_path = row[4]
-	if full_path in ('/nas1/data/psa6644/zen.2456644.18779.xy.uv','/data3/data1/paper/psa/psa228/zen.2440587.69719.uv',
-					'/data3/data1/paper/psa/psa228/zen.2455228.00925.uv',
-					'/data3/data2/home/jacobsda/tmp/vartestspsa/test1/zen.2455820.12187.uv',
-					'/data3/paper/fiducial_night/zen.2455906.07399.uv', '/data3/paper/jkris/560x/5600/zen.2455600.20624.uv',
-					'/data3/paper/jkris/560x/5600/zen.2455600.22016.uv', '/data3/paper/jkris/560x/5600/zen.2455600.26192.uv'):
-		return None
-	if full_path == '/data4/paper/damo/PolImaging/batch_FHD/zen.test.uv':
-		obsnum = 17185731181
-		full_row = row[:5] + [obsnum] + row[6:]
-		return full_row
-	jd = row[2]
-	try:
-		julian_date = round(float('.'.join(row[2].split('.')[1:3]).split('_')[0]), 5)
-	except:
-		julian_date = round(float('.'.join(row[2].split('.')[1:3]).split('_')[0].split('.uv')[0]), 5)
-	try:
-		polarization = row[2].split('.')[3]
-		if polarization not in ('xx', 'xy', 'yx', 'yy'):
-			polarization = 'all'
-	except:
-		polarization = 'all'
-	try:
-		OBS = s.query(pdbi.Observation).filter(pdbi.Observation.julian_date==julian_date).filter(pdbi.Observation.polarization==polarization).one()
-		obsnum = OBS.obsnum
-	except:
-		obsnum = -1
-	full_row = row[:5] + [obsnum] + row[6:]
-
-	return full_row
 
 if __name__ == '__main__':
 	if len(sys.argv) == 3:
@@ -76,5 +58,5 @@ if __name__ == '__main__':
 		backup_obs = '/data4/paper/paperdata_backup/obs_v2.psv'
 		backup_file = '/data4/paper/paperdata_backup/file_v1.psv'
 	
-	#load_backup_obs(backup_obs)
-	load_backup_files(backup_file)
+	#load_backup(backup_obs, table='observation')
+	load_backup(backup_file, table='file')
