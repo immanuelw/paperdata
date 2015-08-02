2b1dae09653101fddb82759c6a6df8820e791d45
yatsu <immwa@sas.upenn.edu>
2015-08-01 22:53:51 -0400

    backup pyganglia to json file

---
+++ pyganglia_dbi/scripts/backup_db.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Load data into MySQL table 
+
+# import the MySQLdb and sys modules
+import sys
+import time
+import os
+import subprocess
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Encoders
+import pyganglia_dbi as pyg
+import json
+import time
+
+### Script to Backup pyganglia database
+### Finds time and date and writes table into .csv file
+
+### Author: Immanuel Washington
+### Date: 8-20-14
+
+import decimal
+def decimal_default(obj):
+	if isinstance(obj, decimal.Decimal):
+		return float(obj)
+
+def json_data(dbo, dump_objects):
+	data = []
+	with open(dbo, 'w') as f:
+		for ser_data in dump_objects.all():
+			s_dict = ser_data.__dict__
+			try:
+				s_dict.pop('_sa_instance_state')
+			except:
+				pass
+			data.append(s_dict)
+		json.dump(data, f, sort_keys=True, indent=1, default=decimal_default)
+	return None
+
+def paperbackup(time_date):
+
+	backup_dir = os.path.join('/data4/paper/pyganglia_backup', str(time_date))
+	if not os.path.isdir(backup_dir):
+		os.mkdir(backup_dir)
+
+	#Create separate files for each directory
+
+	db1 = 'filesystem_{time_date}.json'.format(time_date=time_date)
+	dbo1 = os.path.join(backup_dir, db1)
+	print(dbo1)
+
+	db2 = 'monitor_{time_date}.json'.format(time_date=time_date)
+	dbo2 = os.path.join(backup_dir, db2)
+	print(dbo2)
+
+	db3 = 'iostat_{time_date}.json'.format(time_date=time_date)
+	dbo3 = os.path.join(backup_dir, db3)
+	print(dbo3)
+
+	db4 = 'ram_{time_date}.json'.format(time_date=time_date)
+	dbo4 = os.path.join(backup_dir, db4)
+	print(dbo4)
+
+	db5 = 'cpu_{time_date}.json'.format(time_date=time_date)
+	dbo5 = os.path.join(backup_dir, db5)
+	print(dbo5)
+
+	dbi = pyg.DataBaseInterface()
+	s = dbi.Session()
+
+	FILESYSTEM_dump = s.query(pyg.FILESYSTEM).order_by(pyg.Filesystem.time_date.asc(), pyg.Filesystem.host.asc(), pyg.Filesystem.system.asc())
+	json_data(dbo1, FILESYSTEM_dump)
+
+	MONITOR_dump = s.query(pyg.MONITOR).order_by(pyg.Monitor.time_date.asc(), pyg.Monitor.host.asc(), pyg.Monitor.filename.asc())
+	json_data(dbo2, MONITOR_dump)
+
+	IOSTAT_dump = s.query(pyg.FILESYSTEM).order_by(pyg.Iostat.time_date.asc(), pyg.Iostat.host.asc(), pyg.Iostat.device.asc())
+	json_data(dbo1, IOSTAT_dump)
+
+	RAM_dump = s.query(pyg.FILESYSTEM).order_by(pyg.Ram.time_date.asc(), pyg.Ram.host.asc(), pyg.Ram.polarization.asc())
+	json_data(dbo1, RAM_dump)
+
+	CPU_dump = s.query(pyg.FILESYSTEM).order_by(pyg.Cpu.time_date.asc(), pyg.Cpu.host.asc(), pyg.Cpu.cpu.asc())
+	json_data(dbo1, CPU_dump)
+
+	s.close()
+	print 'Table data backup saved'
+
+	return None
+
+def email_backup(backup_file):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+
+	#Next, log in to the server
+	server.login('paperfeed.pyganglia@gmail.com', 'papercomesfrom1tree')
+
+	msg = MIMEMultipart()
+	msg['Subject'] = 'PAPERDATA TABLE BACKUP'
+	msg['From'] = 'paperfeed.pyganglia@gmail.com'
+	msg['To'] = 'paperfeed.pyganglia@gmail.com'
+
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload(open(backup_file, 'rb').read())
+	Encoders.encode_base64(part)
+	part.add_header('Content-Disposition', 'attachment', filename=backup_file)
+	msg.attach(part)
+
+	#Send the mail
+	server.sendmail('paperfeed.pyganglia@gmail.com', 'paperfeed.pyganglia@gmail.com', msg.as_string())
+
+	server.quit()
+
+	return None
+
+if __name__ == '__main__':
+	time_date = int(time.time())
+
+	paperbackup(time_date)
+	#backup_file = '/data4/paper/pyganglia_backup/{time_date}/pyganglia_backup.json'.format(time_date=time_date)
+	#email_backup(backup_file)
