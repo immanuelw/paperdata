4af08acd39d947163193ceb6f52f08a05890f3d5
yatsu <immwa@sas.upenn.edu>
2015-08-11 17:29:23 -0400

    expand user for tilde

--- paperdata_dbi/__init__.py
+++ paperdata_dbi/__init__.py
@@ -65,11 +65,12 @@ def login_ssh(host, username=None):
 	ssh = paramiko.SSHClient()
 	ssh.load_system_host_keys()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	key_filename = os.path.expanduser('~/.ssh/id_rsa')
 	try:
-		ssh.connect(host, username=username, key_filename='~/.ssh/id_rsa')
+		ssh.connect(host, username=username, key_filename=key_filename)
 	except:
 		try:
-			ssh.connect(host, key_filename='~/.ssh/id_rsa')
+			ssh.connect(host, key_filename=key_filename)
 		except:
 			return None
 
