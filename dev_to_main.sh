fc1cc4933cafb7ed16b782826fa1ab6e8aebc450
yatsu <immwa@sas.upenn.edu>
2015-08-11 17:24:51 -0400

    remove before copy

--- dev_to_main.sh
+++ dev_to_main.sh
@@ -1,3 +1,4 @@
+rm -r paperdata_dbi
 cp -r paperdev_dbi/ paperdata_dbi/
 mv paperdata_dbi/scripts/paperdev_db.py paperdata_dbi/scripts/paperdata_db.py
 sed -i 's/paperdev/paperdata/g' paperdata_dbi/*.py
--- main_to_dev.sh
+++ main_to_dev.sh
@@ -1,3 +1,4 @@
+rm -r paperdev_dbi/
 cp -r paperdata_dbi/ paperdev_dbi/
 mv paperdev_dbi/scripts/paperdata_db.py paperdev_dbi/scripts/paperdev_db.py
 sed -i 's/paperdev/paperdata/g' paperdev_dbi/*.py
