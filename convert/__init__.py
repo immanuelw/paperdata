6e046c1dd68b0c287cdc25d44e5ce341f31da7c5
yatsu <immwa@sas.upenn.edu>
2015-09-01 16:50:43 -0400

    combined modules

--- convert/__init__.py
+++ convert/__init__.py
@@ -4,83 +4,146 @@ import math
 import re
 
 # Formatting conversions:
+MJD_0 = 2400000.5
+MJD_JD2000 = 51544.5
 
+def fpart(x):
+	'''Return fractional part of given number.'''
+	return math.modf(x)[0]
+
+def ipart(x):
+	'''Return integer part of given number.'''
+	return math.modf(x)[1]
+
+def is_leap(year):
+	'''Leap year or not in the Gregorian calendar.'''
+	x = math.fmod(year, 4)
+	y = math.fmod(year, 100)
+	z = math.fmod(year, 400)
+
+	# Divisible by 4 and,
+	# either not divisible by 100 or divisible by 400.
+	return not x and (y or not z)
+
+def gcal2jd(year, month, day, hour=None, minute=None, second=None):
+	year = int(year)
+	month = int(month)
+	day = int(day)
+
+	a = ipart((month - 14) / 12.0)
+	jd = ipart((1461 * (year + 4800 + a)) / 4.0)
+	jd += ipart((367 * (month - 2 - 12 * a)) / 12.0)
+	x = ipart((year + 4900 + a) / 100.0)
+	jd -= ipart((3 * x) / 4.0)
+	jd += day - 2432075.5  # was 32075; add 2400000.5
+
+	jd -= 0.5  # 0 hours; above JD is for midday, switch to midnight.
+	#add to jd for hms 
+	if hour:
+		jd += hour / (24.0)
+	if minute:
+		jd += minute / (24.0 * 60)
+	if second:
+		jd += second / (24.0 * 60 * 60)
+
+	return MJD_0, round(jd, 5)
+
+def jd2gcal(jd1, jd2):
+	from math import modf
+
+	jd1_f, jd1_i = modf(jd1)
+	jd2_f, jd2_i = modf(jd2)
+
+	jd_i = jd1_i + jd2_i
+
+	f = jd1_f + jd2_f
+
+	# Set JD to noon of the current date. Fractional part is the
+	# fraction from midnight of the current date.
+	if -0.5 < f < 0.5:
+		f += 0.5
+	elif f >= 0.5:
+		jd_i += 1
+		f -= 0.5
+	elif f <= -0.5:
+		jd_i -= 1
+		f += 1.5
+
+	l = jd_i + 68569
+	n = ipart((4 * l) / 146097.0)
+	l -= ipart(((146097 * n) + 3) / 4.0)
+	i = ipart((4000 * (l + 1)) / 1461001)
+	l -= ipart((1461 * i) / 4.0) - 31
+	j = ipart((80 * l) / 2447.0)
+	day = l - ipart((2447 * j) / 80.0)
+	l = ipart(j / 11.0)
+	month = j + 2 - (12 * l)
+	year = 100 * (n - 49) + i + l
+
+	return int(year), int(month), int(day), f
+
+def jcal2jd(year, month, day):
+	year = int(year)
+	month = int(month)
+	day = int(day)
+
+	jd = 367 * year
+	x = ipart((month - 9) / 7.0)
+	jd -= ipart((7 * (year + 5001 + x)) / 4.0)
+	jd += ipart((275 * month) / 9.0)
+	jd += day
+	jd += 1729777 - 2400000.5  # Return 240000.5 as first part of JD.
+
+	jd -= 0.5  # Convert midday to midnight.
+
+	return MJD_0, jd
+
+def jd2jcal(jd1, jd2):
+	from math import modf
+
+	jd1_f, jd1_i = modf(jd1)
+	jd2_f, jd2_i = modf(jd2)
+
+	jd_i = jd1_i + jd2_i
+
+	f = jd1_f + jd2_f
+
+	# Set JD to noon of the current date. Fractional part is the
+	# fraction from midnight of the current date.
+	if -0.5 < f < 0.5:
+		f += 0.5
+	elif f >= 0.5:
+		jd_i += 1
+		f -= 0.5
+	elif f <= -0.5:
+		jd_i -= 1
+		f += 1.5
+
+	j = jd_i + 1402.0
+	k = ipart((j - 1) / 1461.0)
+	l = j - (1461.0 * k)
+	n = ipart((l - 1) / 365.0) - ipart(l / 1461.0)
+	i = l - (365.0 * n) + 30.0
+	j = ipart((80.0 * i) / 2447.0)
+	day = i - ipart((2447.0 * j) / 80.0)
+	i = ipart(j / 11.0)
+	month = j + 2 - (12.0 * i)
+	year = (4 * k) + n + i - 4716.0
+
+	return int(year), int(month), int(day), f
 def dec2sex(deci):
-	""" 
-	Converts a Decimal number (in hours or degrees) to Sexagesimal.
-	
-	Parameters
-	----------
-	deci : float
-		A decimal number to be converted to Sexagismal.
-	
-	Returns
-	-------
-	hd : int
-		hours or degrees
-	m : int
-		minutes or arcminutes
-	s : float
-		seconds or arcseconds
-	
-	"""
 	(hfrac, hd) = math.modf(deci)
 	(min_frac, m) = math.modf(hfrac * 60)
 	s = min_frac * 60.
 	return (int(hd), int(m), s)
 
 def sex2dec(hd, min, sec):
-	""" 
-	Converts a Sexagesimal number to a Decimal number.
-	
-	Parameters
-	----------
-	hd : int
-		hours or degrees
-	m : int
-		minutes or arcminutes
-	s : float
-		seconds or arcseconds
-	
-	Returns
-	-------
-	hd : float
-		A decimal number
-	
-	"""
 	return float(hd) + min/60.0 + sec/3600.0
 
 def datetime2decHours(time):
-	""" 
-	Converts a datetime.time or datetime.datetime object into decimal time.
-	
-	Parameters
-	----------
-	time : datetime.time or datetime.datetime
-	
-	Returns
-	-------
-	decTime : float
-		A decimal number representing the input time
-	
-	"""
 	return time.hour + time.minute/60.0 + time.second/3600.0 + time.microsecond/3600000000.0
 
 def string2hours(string):
-	""" 
-	Converts a Sexagesimal string to a decimal hour
-	
-	Parameters
-	----------
-	string : str
-		A string representing a sexagismal time, in the form HH:MM:SS.SS.
-		
-	Returns
-	-------
-	decHours : float
-		The converted sexagismal time to decimal time.
-	
-	"""
 	string = string.strip() # trim leading/trailing whitespace
 
 	div = '[|:|\s]' # allowable field delimiters "|", ":", whitespace
@@ -117,20 +180,6 @@ def string2hours(string):
 	return decHours
 	
 def string2deg(string):
-	""" 
-	Converts a Sexagesimal string to a decimal degrees
-	
-	Parameters
-	----------
-	string : str
-		A string representing a sexagismal time, in the form [+|-]DD:MM:SS.SS
-		
-	Returns
-	-------
-	decHours : float
-		The converted sexagismal degrees to decimal degrees
-	
-	"""
 	string = string.strip() # trim leading/trailing whitespace
 
 	div = '[|:|\s]' # allowable field delimiters "|", ":", whitespace
@@ -170,26 +219,6 @@ def string2deg(string):
 # Date / Time Conversions:
 
 def ymd2jd(year, month, day):
-	""" 
-	Converts a year, month, and day to a Julian Date.
-	This function uses an algorithm from the book "Practical Astronomy with your 
-	Calculator" by Peter Duffet-Smith (Page 7)
-	
-	Parameters
-	----------
-	year : int
-		A Gregorian year
-	month : int
-		A Gregorian month
-	day : int
-		A Gregorian day
-	
-	Returns
-	-------
-	jd : float
-		A Julian Date computed from the input year, month, and day.
-	
-	"""
 	if month == 1 or month == 2:
 		yprime = year - 1
 		mprime = month + 12
@@ -213,23 +242,6 @@ def ymd2jd(year, month, day):
 	return B + C + D + day + 1720994.5
 
 def utcDatetime2gmst(datetimeObj):
-	""" 
-	Converts a Python datetime object with UTC time to Greenwich Mean Sidereal Time.
-	This function uses an algorithm from the book "Practical Astronomy with your 
-	Calculator" by Peter Duffet-Smith (Page 17)
-	
-	Parameters
-	----------
-	datetimeObj : datetime.datetime
-		A Python datetime.datetime object
-	
-	Returns
-	-------
-	< > : datetime.datetime
-		A Python datetime.datetime object corresponding to the Greenwich Mean 
-		Sidereal Time of the input datetime.datetime object. 
-	
-	"""
 	jd = ymd2jd(datetimeObj.year, datetimeObj.month, datetimeObj.day)
 	
 	S = jd - 2451545.0
@@ -246,23 +258,6 @@ def utcDatetime2gmst(datetimeObj):
 	return datetime.datetime(year=datetimeObj.year, month=datetimeObj.month, day=datetimeObj.day, hour=h, minute=m, second=int(s), microsecond=int((s-int(s))*10**6))
 	
 def gmst2utcDatetime(datetimeObj):
-	""" 
-	Converts a Python datetime object representing a Greenwich Mean Sidereal Time 
-	to UTC time. This function uses an algorithm from the book "Practical Astronomy
-	with your Calculator" by Peter Duffet-Smith (Page 18)
-	
-	Parameters
-	----------
-	datetimeObj : datetime.datetime
-		A Python datetime.datetime object
-	
-	Returns
-	-------
-	< > : datetime.datetime
-		A Python datetime.datetime object corresponding to UTC time of the input 
-		Greenwich Mean Sidereal Time datetime.datetime object. 
-	
-	"""
 	jd = ymd2jd(datetimeObj.year, datetimeObj.month, datetimeObj.day)
 	
 	S = jd - 2451545.0
@@ -274,74 +269,16 @@ def gmst2utcDatetime(datetimeObj):
 	UT = GST * 0.9972695663
 	
 	h,m,s = dec2sex(UT)
-	return datetime.datetime(year=datetimeObj.year, month=datetimeObj.month, day=datetimeObj.day, hour=h, minute=m, second=int(s), microsecond=int((s-int(s))*10**6))
+	return datetime.datetime(year=datetimeObj.year, month=datetimeObj.month, day=datetimeObj.day,
+								hour=h, minute=m, second=int(s), microsecond=int((s-int(s))*10**6))
 
 def mjd2jd(mjd):
-	""" 
-	Converts a Modified Julian Date to Julian Date
-	
-	Parameters
-	----------
-	mjd : float (any numeric type)
-		A Modified Julian Date
-	
-	Returns
-	-------
-	jd : float
-		The Julian Date calculated from the input Modified Julian Date
-	 
-	Examples
-	--------
-	>>> mjd2jd(55580.90429)
-	2455581.40429
-	
-	"""
 	return mjd + 2400000.5
 
 def jd2mjd(jd):
-	""" 
-	Converts a Julian Date to a Modified Julian Date
-	
-	Parameters
-	----------
-	jd : float (any numeric type)
-		A julian date
-	
-	Returns
-	-------
-	mjd : float
-		The Modified Julian Date (MJD) calculated from the input Julian Date
-	 
-	Examples
-	--------
-	>>> jd2mjd(2455581.40429)
-	55580.90429
-	
-	"""
 	return float(jd - 2400000.5)
 
 def jd2datetime(jd):
-	""" 
-	Converts a Julian Date to a Python datetime object. The resulting time is in UTC.
-	
-	Parameters
-	----------
-	jd : float (any numeric type)
-		A Julian Date
-	
-	Returns
-	-------
-	< > : datetime.datetime
-		A Python datetime.datetime object calculated using an algorithm from the book
-		"Practical Astronomy with your Calculator" by Peter Duffet-Smith (Page 8)
-	
-	Examples
-	--------
-	>>> jd2datetime(2455581.40429)
-	2011-01-19 21:42:10.000010
-	
-	"""
-	
 	jd = jd + 0.5
 	(jd_frac, jd_int) = math.modf(jd)
 	
@@ -384,118 +321,19 @@ def jd2datetime(jd):
 	return datetime.datetime(y, month, d, h, min, s, ms)
 	
 def mjd2datetime(mjd):
-	""" 
-	Converts a Modified Julian Date to a Python datetime object. The resulting time is in UTC.
-	
-	Parameters
-	----------
-	mjd : float (any numeric type)
-		A Modified Julian Date
-	
-	Returns
-	-------
-	< > : datetime.datetime
-		A Python datetime.datetime object calculated using an algorithm from the book
-		"Practical Astronomy with your Calculator" by Peter Duffet-Smith (Page 8)
-	
-	Examples
-	--------
-	>>> mjd2datetime(55580.90429)
-	2011-01-19 21:42:10.000010
-	
-	"""
 	jd = mjd2jd(mjd)
 	return jd2datetime(jd)
 
 def datetime2jd(datetimeObj):
-	""" 
-	Converts a Python datetime object to a Julian Date.
-	
-	Parameters
-	----------
-	datetimeObj : datetime.datetime
-		A Python datetime.datetime object calculated using an algorithm from the book
-		"Practical Astronomy with your Calculator" by Peter Duffet-Smith (Page 7)
-	
-	Returns
-	-------
-	< > : float 
-		A Julian Date
-	
-	Examples
-	--------
-	>>> datetime2jd(datetimeObject)
-	2455581.40429
-	
-	"""
 	A = ymd2jd(datetimeObj.year, datetimeObj.month, datetimeObj.day)
 	B = datetime2decHours(datetimeObj.time()) / 24.0
 	return A + B
 
 def datetime2mjd(datetimeObj):
-	""" 
-	Converts a Python datetime object to a Modified Julian Date.
-	
-	Parameters
-	----------
-	datetimeObj : datetime.datetime
-		A Python datetime.datetime object calculated using an algorithm from the book
-		"Practical Astronomy with your Calculator" by Peter Duffet-Smith (Page 7)
-	
-	Returns
-	-------
-	< > : float
-		A Modified Julian Date
-	
-	Examples
-	--------
-	>>> datetime2mjd(datetimeObject)
-	55580.90429
-	
-	"""
 	jd = datetime2jd(datetimeObj)
 	return jd2mjd(jd)
 
 def gmst2lst(longitude, hour, minute=None, second=None, lonDirection='W', lonUnits='DEGREES'):
-	""" 
-	Converts Greenwich Mean Sidereal Time to Local Sidereal Time. 
-	
-	Parameters
-	----------
-	longitude : float (any numeric type)
-		The longitude of the site to calculate the Local Sidereal Time. Defaults are
-		Longitude WEST and units DEGREES, but these can be changed with the optional 
-		parameters lonDirection and lonUnits.
-	hour : int (or float)
-		If an integer, the function will expect a minute and second. If a float, it
-		will ignore minute and second and convert from decimal hours to hh:mm:ss.
-	minute : int
-		Ignored if hour is a float.
-	second : int (any numeric type, to include microseconds)
-		Ignored if hour is a float.
-	lonDirection : string
-		Default is longitude WEST, 'W', but you can specify EAST by passing 'E'.
-	lonUnits : string
-		Default units are 'DEGREES', but this can be switched to radians by passing
-		'RADIANS' in this parameter.
-	
-	Returns
-	-------
-	hour : int
-		The hour of the calculated LST
-	minute : int
-		The minutes of the calculated LST
-	second: float
-		The seconds of the calculated LST
-	
-	Examples
-	--------
-	>>> gmst2lst(70.3425, hour=14, minute=26, second=18)
-	(9, 44, 55.80000000000126)
-	>>> gmst2lst(5.055477, hour=14.4383333333333333, lonDirection='E', lonUnits='RADIANS')
-	(9, 44, 55.79892611013463)
-	
-	"""
 	if minute != None and second != None:
 		hours = sex2dec(hour, minute, second)
 	elif minute == None and second == None:
@@ -520,45 +358,6 @@ def gmst2lst(longitude, hour, minute=None, second=None, lonDirection='W', lonUni
 	return dec2sex(lst)
 	
 def lst2gmst(longitude, hour, minute=None, second=None, lonDirection='W', lonUnits='DEGREES'):
-	""" 
-	Converts Local Sidereal Time to Greenwich Mean Sidereal Time.
-	
-	Parameters
-	----------
-	longitude : float (any numeric type)
-		The longitude of the site to calculate the Local Sidereal Time. Defaults are
-		Longitude WEST and units DEGREES, but these can be changed with the optional 
-		parameters lonDirection and lonUnits.
-	hour : int (or float)
-		If an integer, the function will expect a minute and second. If a float, it
-		will ignore minute and second and convert from decimal hours to hh:mm:ss.
-	minute : int
-		Ignored if hour is a float.
-	second : int (any numeric type, to include microseconds)
-		Ignored if hour is a float.
-	lonDirection : string
-		Default is longitude WEST, 'W', but you can specify EAST by passing 'E'.
-	lonUnits : string
-		Default units are 'DEGREES', but this can be switched to radians by passing
-		'RADIANS' in this parameter.
-	
-	Returns
-	-------
-	hour : int
-		The hour of the calculated GMST
-	minute : int
-		The minutes of the calculated GMST
-	second: float
-		The seconds of the calculated GMST
-	
-	Examples
-	--------
-	>>> lst2gmst(70.3425, hour=14, minute=26, second=18)
-	(19, 7, 40.20000000000607)
-	>>> lst2gmst(5.055477, hour=14.4383333333333333, longitudeDirection='E', longitudeUnits='RADIANS')
-	(19, 7, 40.20107388985991)
-	
-	"""
 	if minute != None and second != None:
 		hours = sex2dec(hour, minute, second)
 	elif minute == None and second == None:
@@ -583,33 +382,7 @@ def lst2gmst(longitude, hour, minute=None, second=None, lonDirection='W', lonUni
 	return dec2sex(gmst)
 
 def mjdLon2lst(mjd, longitude, lonDirection='W', lonUnits='DEGREES'):
-	""" 
-	Converts an MJD and a Longitude to Local Sidereal Time
-	
-	Parameters
-	----------
-	mjd : float (any numeric type)
-		A Modified Julian Date for which to compute GMST.
-	longitude : float (any numeric type)
-		The longitude of the site to calculate the Local Sidereal Time. Defaults are
-		Longitude WEST and units DEGREES, but these can be changed with the optional 
-		parameters lonDirection and lonUnits.
-	lonDirection : string
-		Default is longitude WEST, 'W', but you can specify EAST by passing 'E'.
-	lonUnits : string
-		Default units are 'DEGREES', but this can be switched to radians by passing
-		'RADIANS' in this parameter.
-	
-	Returns
-	-------
-	hour : int
-		The hour of the calculated LST
-	minute : int
-		The minutes of the calculated LST
-	second: float
-		The seconds of the calculated LST
-	
-	"""
 	dt = mjd2datetime(mjd)
 	gmst = utcDatetime2gmst(dt)
-	return gmst2lst(longitude, hour=gmst.hour, minute=gmst.minute, second=(gmst.second+gmst.microsecond/10**6), lonDirection=lonDirection, lonUnits=lonUnits)
+	return gmst2lst(longitude, hour=gmst.hour, minute=gmst.minute, second=(gmst.second+gmst.microsecond/10**6),
+					lonDirection=lonDirection, lonUnits=lonUnits)
