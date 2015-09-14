cf258cea23d25026c600657f523fe8c02267d9ca
yatsu <immwa@sas.upenn.edu>
2015-09-14 15:57:33 -0400

    unnecessary

--- main_to_dev.sh
+++
@@ -1,5 +0,0 @@
-rm -r paperdev_dbi/
-cp -r paperdata_dbi/ paperdev_dbi/
-mv paperdev_dbi/scripts/paperdata_db.py paperdev_dbi/scripts/paperdev_db.py
-sed -i 's/paperdata/paperdev/g' paperdev_dbi/*.py
-sed -i 's/paperdata/paperdev/g' paperdev_dbi/scripts/*.py
