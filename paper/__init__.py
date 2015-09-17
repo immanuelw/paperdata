d6bbe2681bb81c8c2a6b0af9337965c4a8e03970
yatsu <immwa@sas.upenn.edu>
2015-09-16 21:44:27 -0400

    documentation and DRY

--- paper/__init__.py
+++ paper/__init__.py
@@ -11,9 +11,13 @@ try:
 except:
 	import ConfigParser as configparser
 
-#SSH/SFTP Function
-#Need private key so don't need username/password
 def login_ssh(host, username=None):
+	'''
+	SSH/SFTP connection to remote host
+
+	input: remote host and username
+	output: ssh connection to be used to run commands to remote host
+	'''
 	ssh = paramiko.SSHClient()
 	ssh.load_system_host_keys()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -32,6 +36,12 @@ logger = logging.getLogger('paper')
 Base = declarative_base()
 
 class DictFix(object):
+	'''
+	superclass for all SQLAlchemy Table objects
+	allows access to object row dictionary
+
+	output: dict of table attributes
+	'''
 	def to_dict(self):
 		try:
 			new_dict = {}
@@ -43,14 +53,12 @@ class DictFix(object):
 
 class DataBaseInterface(object):
 	def __init__(self, configfile='~/paperdata.cfg'):
-		"""
+		'''
 		Connect to the database and initiate a session creator.
-		 or
-		create a FALSE database
+		superclass of DBI for paperdata, paperdev, and ganglia databases
 
-		db.cfg is the default setup. Config files live in ddr_compress/configs
-		To use a config file, copy the desired file ~/.paperstill/db.cfg
-		"""
+		input: configuration file -- defaults to paperdata.cfg, can be changed on initiation
+		'''
 		if not configfile is None:
 			config = configparser.ConfigParser()
 			configfile = os.path.expanduser(configfile)
@@ -80,57 +88,58 @@ class DataBaseInterface(object):
 		self.Session = sessionmaker(bind=self.engine)
 
 	def create_table(Table):
-		"""
+		'''
 		creates a table in the database.
-		"""
+		input: table object
+		'''
 		Table.__table__.create(bind=self.engine)
 
-	def get_entry(self, TABLE, unique_value):
-		"""
+	def get_entry(self, s=None, TABLE, unique_value):
+		'''
 		retrieves any object.
 		Errors if there are more than one of the same object in the db. This is bad and should
 		never happen
 
-		todo:test
-		"""
-		s = self.Session()
+		input: session object(optional), table name, primary key value of row
+		output: table object
+		'''
+		if s is None:
+			s = self.Session()
+			open_sess = True
 		table = getattr(sys.modules[__name__], TABLE.title())
 		try:
 			ENTRY = s.query(table).get(unique_value)
 		except:
 			return None
-		s.close()
+		if open_sess:
+			s.close()
 		return ENTRY
 
-	def update_entry(self, ENTRY):
-		"""
-		updates any object field
-		***NEED TO TEST
-		"""
-		s = self.Session()
-		s.add(ENTRY)
-		s.commit()
-		s.close()
-		return True
-
 	def set_entry(self, ENTRY, field, new_value):
-		"""
+		'''
 		sets the value of any entry
 		input: ENTRY object, field to be changed, new value
-		"""
+		'''
 		setattr(ENTRY, field, new_value)
-		yay = self.update_entry(ENTRY)
+		yay = self.add_entry(ENTRY)
 		return yay
 
-	def add_entry(self, ENTRY):
-		s = self.Session()
+	def add_entry(self, s=None, ENTRY):
+		'''
+		adds entry to database and commits
+		does not add if duplicate found
+
+		input: session object(optional), table object
+		'''
+		if s is None:
+			s = self.Session()
+			open_sess = True
 		try:
 			s.add(ENTRY)
 			s.commit()
 		except (exc.IntegrityError):
 			s.rollback()
-			s.close()
 			print('Duplicate entry found ... skipping entry')
-			return None
-		s.close()
+		if open_sess:
+			s.close()
 		return None
