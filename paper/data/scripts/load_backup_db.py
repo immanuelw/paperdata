d43fe2645d549c98530e4522a424187962af3d0c
yatsu <immwa@sas.upenn.edu>
2015-09-19 01:35:12 -0400

    simplify

--- paper/data/scripts/load_backup_db.py
+++ paper/data/scripts/load_backup_db.py
@@ -15,58 +15,44 @@ import sqlalchemy.exc
 ### Author: Immanuel Washington
 ### Date: 5-06-15
 
-def load_backup(backup, table=None):
+def load_backup(backup_file=None, table=None):
 	'''
 	loads backups from json into database
 
 	input: name of backup file, table name
 	'''
+	if table is None:
+		return None
+	if backup_file is None:
+		backup_list = glob.glob('/data4/paper/paperdata_backup/[0-9]*')
+		backup_list.sort(reverse=True)
+		timestamp = int(backup_list[0].split('/')[-1])
+		backup_file = '/data4/paper/paperdata_backup/{timestamp}/{table}_{timestamp}.json'.format(table=table, timestamp=timestamp)
+
 	dbi = pdbi.DataBaseInterface()
 	s = dbi.Session()
 	with open(backup, 'r') as backup_db:
 		read = json.load(backup_db)
-		if table is None:
-			return None
-		else:
-			for row in read:
-				print(row.items())
-				try:
-					if table == 'observation':
-						dbi.add_to_table(s, 'observation', row)
-					elif table == 'file':
-						dbi.add_to_table(s, 'file', row)
-					#elif table == 'feed':
-					#	dbi.add_to_table(s, 'feed', row)
-					elif table == 'log':
-						dbi.add_to_table(s, 'log', row)
-					#elif table == 'rtp_file':
-					#	dbi.add_to_table(s, 'rtp_file', row)
-				except KeyboardInterrupt:
-					raise
-				except:
-					print('Failed to load in entry')
+		for row in read:
+			print(row.items())
+			try:
+				dbi.add_to_table(s, table, row)
+			except KeyboardInterrupt:
+				raise
+			except:
+				print('Failed to load in entry')
 	s.close()
 
 	return None
 
 if __name__ == '__main__':
 	if len(sys.argv) == 3:
-		backup_obs = sys.argv[1]
+		backup_table = sys.argv[1]
 		backup_file = sys.argv[2]
+		load_backup(backup_file, table=backup_table)
 	else:
-		backup_list = glob.glob('/data4/paper/paper_backup/[0-9]*')
-		backup_list.sort(reverse=True)
-		backup_dir = backup_list[0]
-		timestamp = int(backup_dir.split('/')[-1])
-		backup_obs = '/data4/paper/paperdata_backup/{timestamp}/obs_{timestamp}.json'.format(timestamp=timestamp)
-		backup_file = '/data4/paper/paperdata_backup/{timestamp}/file_{timestamp}.json'.format(timestamp=timestamp)
-		backup_feed = '/data4/paper/paperdata_backup/{timestamp}/feed_{timestamp}.json'.format(timestamp=timestamp)
-		backup_log = '/data4/paper/paperdata_backup/{timestamp}/log_{timestamp}.json'.format(timestamp=timestamp)
-		#backup_rtp_file = '/data4/paper/paperdata_backup/{timestamp}/rtp_file_{timestamp}.json'.format(timestamp=timestamp)
-		
-	
-	#load_backup(backup_obs, table='observation')
-	load_backup(backup_file, table='file')
-	#load_backup(backup_feed, table='feed')
-	#load_backup(backup_log, table='log')
-	#load_backup(backup_rtp, table='rtp_file'
+		#load_backup(table='observation')
+		load_backup(table='file')
+		#load_backup(table='feed')
+		#load_backup(table='log')
+		#load_backup(table='rtp_file')
