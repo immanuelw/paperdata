bd1d907fa7e9bbdca9c75d17e74151930ca29cba
yatsu <immwa@sas.upenn.edu>
2015-09-19 21:22:08 -0400

    fix

--- paper/site/admin/manage.py
+++ paper/site/admin/manage.py
@@ -1,14 +1,14 @@
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from paper.site.search.flask_app import app, db
+from paper.site.admin.flask_app import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
-from app.user_command import UserCommand
+from paper.site.admin.user_command import UserCommand
 manager.add_command('user', UserCommand)
 
 if __name__ == '__main__':
