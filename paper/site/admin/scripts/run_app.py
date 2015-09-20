b0f576e375cb2ed740c736addc87f22363dcf600
yatsu <immwa@sas.upenn.edu>
2015-09-19 21:25:54 -0400

    fixes;

--- paper/site/admin/scripts/run_app.py
+++ paper/site/admin/scripts/run_app.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 
-from paper.site.search.flask_app import app
-from paper.site.search import views, models, sets, comments, data_sources, filters, users
+from paper.site.admin.flask_app import app
+from paper.site.admin import views, models, comments, users
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
--- paper/site/admin/users.py
+++ paper/site/admin/users.py
@@ -8,7 +8,7 @@ import re
 
 @lm.user_loader
 def load_user(id):
-	user = db_utils.query(database='eorlive', table='user',	field_tuples=(('username', '==', id),))[0]
+	user = db_utils.query(database='admin', table='user',	field_tuples=(('username', '==', id),))[0]
 	return user
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -20,7 +20,7 @@ def login():
 		username = request.form['username'].strip()
 		password = request.form['password'].strip()
 
-		u = db_utils.query(database='eorlive', table='user', field_tuples=(('username', '==', username),))[0]
+		u = db_utils.query(database='admin', table='user', field_tuples=(('username', '==', username),))[0]
 
 		password = password.encode('UTF-8')
 		if not u:
@@ -46,7 +46,7 @@ def signup():
 		fname = request.form['fname'].strip()
 		lname = request.form['lname'].strip()
 
-		testU = db_utils.query(database='eorlive', table='user', field_tuples=(('username', '==', username),))[0]
+		testU = db_utils.query(database='admin', table='user', field_tuples=(('username', '==', username),))[0]
 
 		if password != password2:
 			error = 'Passwords must be the same.'
@@ -65,7 +65,7 @@ def signup():
 			db.session.refresh(new_user)
 			db.session.commit()
 
-			u = db_utils.query(database='eorlive', table='user', field_tuples=(('username', '==', username),))[0]
+			u = db_utils.query(database='admin', table='user', field_tuples=(('username', '==', username),))[0]
 
 			login_user(u)
 			flash('You were logged in', 'flash')
@@ -89,7 +89,7 @@ def delete_user():
 		for aSet in setList:
 			field_tuples = field_tuple_base + (('id', '==', getattr(aSet, 'id')),)
 
-			theSet = db_utils.query(database='eorlive', table='set', field_tuples=field_tuples)[0]
+			theSet = db_utils.query(database='admin', table='set', field_tuples=field_tuples)[0]
 
 			if action == 'transfer':
 				setattr(theSet, 'username', g.user.username)
@@ -98,7 +98,7 @@ def delete_user():
 				db.session.delete(theSet)
 			db.session.commit()
 
-		u = db_utils.query(database='eorlive', table='user', field_tuples=(('username', '==', username),))[0]
+		u = db_utils.query(database='admin', table='user', field_tuples=(('username', '==', username),))[0]
 
 		db.session.delete(u)
 		db.session.commit()
