51d28dc398adf9de1de3d77f8622cbec114ce7d3
yatsu <immwa@sas.upenn.edu>
2015-09-28 00:22:37 -0400

    redundant

--- paper/__init__.py
+++ paper/__init__.py
@@ -40,27 +40,6 @@ def ssh_scope(host, username=None):
 	finally:
 		ssh.close()
 
-def login_ssh(host, username=None):
-	'''
-	SSH/SFTP connection to remote host
-
-	input: remote host and username
-	output: ssh connection to be used to run commands to remote host
-	'''
-	ssh = paramiko.SSHClient()
-	ssh.load_system_host_keys()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	key_filename = os.path.expanduser('~/.ssh/id_rsa')
-	try:
-		ssh.connect(host, username=username, key_filename=key_filename)
-	except:
-		try:
-			ssh.connect(host, key_filename=key_filename)
-		except:
-			return None
-
-	return ssh
-
 logger = logging.getLogger('paper')
 Base = declarative_base()
 
