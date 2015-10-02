eadd4ab07f9ce2693b3014cefc0620cdf7026772
yatsu <immwa@sas.upenn.edu>
2015-10-02 11:16:35 -0400

    documentation

--- paper/site/search/scripts/pull_chart_data.py
+++ paper/site/search/scripts/pull_chart_data.py
@@ -16,7 +16,8 @@ def write_to_log(msg):
 	'''
 	write message to log
 
-	input: message
+	Args:
+		msg (str): message
 	'''
 	print(msg)
 
@@ -24,20 +25,24 @@ def profile():
 	'''
 	profile the time to run certain functions
 
-	output: time taken by function in seconds
+	Returns:
+		int: time taken by function in seconds
 	'''
 	global profiling_mark
 	result = datetime.now() - profiling_mark
 	profiling_mark = datetime.now()
+
 	return result.total_seconds()
 
 def log_query_time(var_name):
 	'''
 	write time taken to query database to log
 
-	input: variable name
+	Args:
+		var_name (str): variable name
 	'''
 	write_to_log('{var_name} query ran in {profile_time} seconds'.format(var_name=var_name, profile_time=profile())
+
 	return None
 
 def update():
