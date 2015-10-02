f587dccfdb904a96e573ddfa18f3e2a3ae09b946
yatsu <immwa@sas.upenn.edu>
2015-10-02 13:18:08 -0400

    changed order

--- paper/ganglia/scripts/add_data.py
+++ paper/ganglia/scripts/add_data.py
@@ -276,20 +276,20 @@ def add_data(ssh, host):
 		with dbi.session_scope() as s:
 			iostat_all_data = iostat(ssh, host)
 			for name, iostat_data in iostat_all_data.items():
-				dbi.add_to_table('iostat', iostat_data, s)
+				dbi.add_to_table(s, 'iostat', iostat_data)
 
 			ram_data = ram_free(ssh, host)
-			dbi.add_to_table('ram', ram_data, s)
+			dbi.add_to_table(s, 'ram', ram_data)
 
 			cpu_all_data = cpu_perc(ssh, host)
 			for key, cpu_data in cpu_all_data.items():
-				dbi.add_to_table('cpu', cpu_data, s)
+				dbi.add_to_table(s, 'cpu', cpu_data)
 
 			if host in ('folio',):
 				paths = ('/data3', '/data4')
 				for path in paths:
 					system_data = filesystem(ssh, path)
-					dbi.add_to_table('filesystem', system_data, s)
+					dbi.add_to_table(s, 'filesystem', system_data)
 
 	return None
 
--- paper/ganglia/scripts/monitor_folio_still.py
+++ paper/ganglia/scripts/monitor_folio_still.py
@@ -143,7 +143,7 @@ try:
 				file_time.update({filename:time.time()})
 		with pyg_dbi.session_scope as s:
 			for monitor_data in file_log:
-				pyg_dbi.add_to_table('monitor', monitor_data, s)
+				pyg_dbi.add_to_table(s, 'monitor', monitor_data)
 		file_log = []
 		s.close()
 		statusscr.refresh()
