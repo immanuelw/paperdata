98fda206ea86cde6402c6f36a1ba900cd7f0e95d
yatsu <immwa@sas.upenn.edu>
2015-10-02 21:10:47 -0400

    changed to dicts

--- paper/data/data_db.py
+++ paper/data/data_db.py
@@ -23,20 +23,33 @@ class file:
 						'write_to_tape',
 						'delete_file',
 						'timestamp')
-		self.db_descr = {'host':('VARCHAR(100)', 'None', 'No', 'host of file system that file is located on'),
-						'path':('VARCHAR(100)', 'None', 'No', 'directory that file is located in'),
-						'filename':('VARCHAR(100)', 'None', 'No', 'name of file (ex: zen.2446321.16617.uvcRRE)'),
-						'filetype':('VARCHAR(20)', 'None', 'No', 'filetype (ex: uv, uvcRRE, npz)'),
-						'full_path':('VARCHAR(200)', 'None', 'Primary',
-										'combination of host, path, and filename which is a unique identifier for each file'),
-						'obsnum':('BIGINT', 'None', 'Foreign', 'observation number used to track files using integer'),
-						'filesize':('DECIMAL(7, 2)', 'None', 'No', 'size of file in megabytes'),
-						'md5sum':('INTEGER', 'None', 'No', '32-bit integer md5 checksum of file'),
-						'tape_index':('VARCHAR(100)', 'None', 'No', 'indexed location of file on tape'),
-						'source_host':('VARCHAR(100)', 'None', 'No', 'original source(host) of file'),
-						'write_to_tape':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file needs to be written to tape'),
-						'delete_file':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file needs to be deleted from its host'),
-						'timestamp':('BIGINT', 'None', 'No', 'time entry was last updated')}
+		self.db_descr = {'host': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'host of file system that file is located on'},
+						'path': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'directory that file is located in'},
+						'filename': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'name of file (ex: zen.2446321.16617.uvcRRE)'},
+						'filetype': {'type': 'VARCHAR(20)', 'default': 'None',
+						'primary key': 'No', 'description': 'filetype (ex: uv, uvcRRE, npz)'},
+						'full_path': {'type': 'VARCHAR(200)', 'default': 'None',
+						'primary key': 'Primary', 'description':
+													'combination of host, path, and filename which is a unique identifier for each file'},
+						'obsnum': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Foreign', 'description': 'observation number used to track files using integer'},
+						'filesize': {'type': 'DECIMAL(7, 2)', 'default': 'None',
+						'primary key': 'No', 'description': 'size of file in megabytes'},
+						'md5sum': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': '32-bit integer md5 checksum of file'},
+						'tape_index': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'indexed location of file on tape'},
+						'source_host': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'original source(host) of file'},
+						'write_to_tape': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicated whether file needs to be written to tape'},
+						'delete_file': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicated whether file needs to be deleted from its host'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time entry was last updated'}}
 
 class observation:
 	def __init__(self):
@@ -56,21 +69,36 @@ class observation:
 						'next_obs',
 						'edge',
 						'timestamp')
-		self.db_descr = {'obsnum':('BIGINT', 'None', 'Primary', 'observation number used to track files using integer'),
-						'julian_date':('DECIMAL(12, 5)', 'None', 'No', 'julian date of observation'),
-						'polarization':('VARCHAR(4)', 'None', 'No', 'polarization of observation'),
-						'julian_day':('INTEGER', 'None', 'No', 'integer value of julian date'),
-						'lst':('DECIMAL(3, 1)', 'None', 'No', 'local sidereal time for south africa at julian date'),
-						'era':('INTEGER', 'None', 'No', 'era of observation taken: 32, 64, 128'),
-						'era_type':('VARCHAR(20)', 'None', 'No', 'type of observation taken: dual pol, etc.'),
-						'length':('DECIMAL(6, 5)', 'None', 'No', 'length of time data was taken for particular observation'),
-						'time_start':('DECIMAL(12, 5)', 'None', 'No', 'start time of observation'),
-						'time_end':('DECIMAL(12, 5)', 'None', 'No', 'end time of observation'),
-						'delta_time':('DECIMAL(12, 5)', 'None', 'No', 'time step of observation'),
-						'prev_obs':('BIGINT', 'None', 'Unique', 'observation number of previous observation'),
-						'next_obs':('BIGINT', 'None', 'Unique', 'observation number of next observation'),
-						'edge':('BOOLEAN', 'None', 'No', 'boolean value indicating if observation at beginning/end of night or not'),
-						'timestamp':('BIGINT', 'None', 'No', 'time entry was last updated')}
+		self.db_descr = {'obsnum': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Primary', 'description': 'observation number used to track files using integer'},
+						'julian_date': {'type': 'DECIMAL(12, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'julian date of observation'},
+						'polarization': {'type': 'VARCHAR(4)', 'default': 'None',
+						'primary key': 'No', 'description': 'polarization of observation'},
+						'julian_day': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'integer value of julian date'},
+						'lst': {'type': 'DECIMAL(3, 1)', 'default': 'None',
+						'primary key': 'No', 'description': 'local sidereal time for south africa at julian date'},
+						'era': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'era of observation taken: 32, 64, 128'},
+						'era_type': {'type': 'VARCHAR(20)', 'default': 'None',
+						'primary key': 'No', 'description': 'type of observation taken: dual pol, etc.'},
+						'length': {'type': 'DECIMAL(6, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'length of time data was taken for particular observation'},
+						'time_start': {'type': 'DECIMAL(12, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'start time of observation'},
+						'time_end': {'type': 'DECIMAL(12, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'end time of observation'},
+						'delta_time': {'type': 'DECIMAL(12, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'time step of observation'},
+						'prev_obs': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Unique', 'description': 'observation number of previous observation'},
+						'next_obs': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Unique', 'description': 'observation number of next observation'},
+						'edge': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicating if observation at beginning/end of night or not'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time entry was last updated'}}
 
 class feed:
 	def __init__(self):
@@ -83,15 +111,23 @@ class feed:
 						'ready_to_move',
 						'moved_to_distill',
 						'timestamp')
-		self.db_descr = {'host':('VARCHAR(100)', 'None', 'No', 'host of file system that file is located on'),
-						'path':('VARCHAR(100)', 'None', 'No', 'directory that file is located in'),
-						'filename':('VARCHAR(100)', 'None', 'No', 'name of file (ex: zen.2446321.16617.uvcRRE)'),
-						'full_path':('VARCHAR(200)', 'None', 'Primary',
-										'combination of host, path, and filename which is a unique identifier for each file'),
-						'julian_day':('INTEGER', 'None', 'No', 'integer value of julian date'),
-						'ready_to_move':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file is ready to be moved to distill'),
-						'moved_to_distill':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file has been moved to distill yet'),
-						'timestamp':('BIGINT', 'None', 'No', 'time entry was last updated')}
+		self.db_descr = {'host': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'host of file system that file is located on'},
+						'path': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'directory that file is located in'},
+						'filename': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'name of file (ex: zen.2446321.16617.uvcRRE)'},
+						'full_path': {'type': 'VARCHAR(200)', 'default': 'None',
+						'primary key': 'Primary', 'description':
+													'combination of host, path, and filename which is a unique identifier for each file'},
+						'julian_day': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'integer value of julian date'},
+						'ready_to_move': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicated whether file is ready to be moved to distill'},
+						'moved_to_distill': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicated whether file has been moved to distill yet'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time entry was last updated'}}
 
 class log:
 	def __init__(self):
@@ -100,10 +136,14 @@ class log:
 						'table',
 						'identifier',
 						'timestamp')
-		self.db_descr = {'action':('VARCHAR(100)', 'None', 'No', 'action taken by script'),
-						'table':('VARCHAR(100)', 'None', 'No', 'table script is acting on'),
-						'identifier':('VARCHAR(200)', 'None', 'No', 'primary key of item that was changed'),
-						'timestamp':('BIGINT', 'None', 'No', 'time action was taken')}
+		self.db_descr = {'action': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'action taken by script'},
+						'table': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'table script is acting on'},
+						'identifier': {'type': 'VARCHAR(200)', 'default': 'None',
+						'primary key': 'No', 'description': 'primary key of item that was changed'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time action was taken'}}
 
 class rtp_file:
 	def __init__(self):
@@ -120,19 +160,31 @@ class rtp_file:
 						'new_host',
 						'new_path',
 						'timestamp')
-		self.db_descr = {'host':('VARCHAR(100)', 'None', 'No', 'host of file system that file is located on'),
-						'path':('VARCHAR(100)', 'None', 'No', 'directory that file is located in'),
-						'filename':('VARCHAR(100)', 'None', 'No', 'name of file (ex: zen.2446321.16617.uvcRRE)'),
-						'filetype':('VARCHAR(20)', 'None', 'No', 'filetype (ex: uv, uvcRRE, npz)'),
-						'full_path':('VARCHAR(200)', 'None', 'Primary',
-										'combination of host, path, and filename which is a unique identifier for each file'),
-						'obsnum':('BIGINT', 'None', 'Foreign', 'observation number used to track files using integer'),
-						'transferred':('BOOLEAN', 'None', 'No', 'boolean value indicated whether file has bee copied to USDB'),
-						'md5sum':('INTEGER', 'None', 'No', '32-bit integer md5 checksum of file'),
-						'julian_day':('INTEGER', 'None', 'No', 'integer value of julian date'),
-						'new_host':('VARCHAR(100)', 'None', 'No', 'new source(host) of file'),
-						'new_path':('VARCHAR(100)', 'None', 'No', 'new path of file of new host'),
-						'timestamp':('BIGINT', 'None', 'No', 'time entry was last updated')}
+		self.db_descr = {'host': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'host of file system that file is located on'},
+						'path': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'directory that file is located in'},
+						'filename': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'name of file (ex: zen.2446321.16617.uvcRRE)'},
+						'filetype': {'type': 'VARCHAR(20)', 'default': 'None',
+						'primary key': 'No', 'description': 'filetype (ex: uv, uvcRRE, npz)'},
+						'full_path': {'type': 'VARCHAR(200)', 'default': 'None',
+						'primary key': 'Primary', 'description':
+													'combination of host, path, and filename which is a unique identifier for each file'},
+						'obsnum': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Foreign', 'description': 'observation number used to track files using integer'},
+						'transferred': {'type': 'BOOLEAN', 'default': 'None',
+						'primary key': 'No', 'description': 'boolean value indicated whether file has bee copied to USDB'},
+						'md5sum': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': '32-bit integer md5 checksum of file'},
+						'julian_day': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'integer value of julian date'},
+						'new_host': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'new source(host) of file'},
+						'new_path': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'new path of file of new host'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time entry was last updated'}}
 
 class rtp_observation:
 	def __init__(self):
@@ -146,15 +198,24 @@ class rtp_observation:
 						'prev_obs',
 						'next_obs',
 						'timestamp')
-		self.db_descr = {'obsnum':('BIGINT', 'None', 'Primary', 'rtp_observation number used to track files using integer'),
-						'julian_date':('DECIMAL(12, 5)', 'None', 'No', 'julian date of rtp_observation'),
-						'polarization':('VARCHAR(4)', 'None', 'No', 'polarization of rtp_observation'),
-						'julian_day':('INTEGER', 'None', 'No', 'integer value of julian date'),
-						'era':('INTEGER', 'None', 'No', 'era of rtp_observation taken: 32, 64, 128'),
-						'length':('DECIMAL(6, 5)', 'None', 'No', 'length of time data was taken for particular rtp_observation'),
-						'prev_obs':('BIGINT', 'None', 'Unique', 'rtp_observation number of previous rtp_observation'),
-						'next_obs':('BIGINT', 'None', 'Unique', 'rtp_observation number of next rtp_observation'),
-						'timestamp':('BIGINT', 'None', 'No', 'time entry was last updated')}
+		self.db_descr = {'obsnum': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Primary', 'description': 'rtp_observation number used to track files using integer'},
+						'julian_date': {'type': 'DECIMAL(12, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'julian date of rtp_observation'},
+						'polarization': {'type': 'VARCHAR(4)', 'default': 'None',
+						'primary key': 'No', 'description': 'polarization of rtp_observation'},
+						'julian_day': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'integer value of julian date'},
+						'era': {'type': 'INTEGER', 'default': 'None',
+						'primary key': 'No', 'description': 'era of rtp_observation taken: 32, 64, 128'},
+						'length': {'type': 'DECIMAL(6, 5)', 'default': 'None',
+						'primary key': 'No', 'description': 'length of time data was taken for particular rtp_observation'},
+						'prev_obs': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Unique', 'description': 'rtp_observation number of previous rtp_observation'},
+						'next_obs': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'Unique', 'description': 'rtp_observation number of next rtp_observation'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time entry was last updated'}}
 
 class rtp_log:
 	def __init__(self):
@@ -163,10 +224,14 @@ class rtp_log:
 						'table',
 						'identifier',
 						'timestamp')
-		self.db_descr = {'action':('VARCHAR(100)', 'None', 'No', 'action taken by script'),
-						'table':('VARCHAR(100)', 'None', 'No', 'table script is acting on'),
-						'identifier':('VARCHAR(200)', 'None', 'No', 'primary key of item that was changed'),
-						'timestamp':('BIGINT', 'None', 'No', 'time action was taken')}
+		self.db_descr = {'action': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'action taken by script'},
+						'table': {'type': 'VARCHAR(100)', 'default': 'None',
+						'primary key': 'No', 'description': 'table script is acting on'},
+						'identifier': {'type': 'VARCHAR(200)', 'default': 'None',
+						'primary key': 'No', 'description': 'primary key of item that was changed'},
+						'timestamp': {'type': 'BIGINT', 'default': 'None',
+						'primary key': 'No', 'description': 'time action was taken'}}
 
 #dictionary of instantiated classes
 instant_class = {'file':file(),
