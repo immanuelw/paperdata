b9277af369a8298f307c3e59a69b03219fd144c7
yatsu <immwa@sas.upenn.edu>
2015-10-08 17:05:15 -0400

    my own distiller search

---
+++ paper/distiller/dbi.py
@@ -0,0 +1,102 @@
+from sqlalchemy import Table, Column, String, Integer, ForeignKey, Float,func,DateTime,Enum,BigInteger,Numeric,Text
+from sqlalchemy import Table, Column, String, Integer, ForeignKey, Float, func, Boolean, DateTime, Enum, BigInteger, Numeric, Text
+from sqlalchemy import event, DDL, UniqueConstraint, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship, backref
+import os, sys, logging
+import paper as ppdata
+
+Base = ppdata.Base
+logger = logging.getLogger('paper.distiller')
+
+FILE_PROCESSING_STAGES = ('NEW','UV_POT', 'UV', 'UVC', 'CLEAN_UV', 'UVCR', 'CLEAN_UVC',
+							'ACQUIRE_NEIGHBORS', 'UVCRE', 'NPZ', 'UVCRR', 'NPZ_POT', 'CLEAN_UVCRE', 'UVCRRE',
+							'CLEAN_UVCRR', 'CLEAN_NPZ', 'CLEAN_NEIGHBORS', 'UVCRRE_POT', 'CLEAN_UVCRRE', 'CLEAN_UVCR','COMPLETE')`
+#############
+#
+#   The basic definition of our database
+#
+########
+
+class Observation(Base):
+	__tablename__ = 'observation'
+	julian_date = Column(Numeric(16,8))
+	pol = Column(String(4))
+	obsnum = Column(BigInteger, primary_key=True)
+	status = Column(Enum(*FILE_PROCESSING_STAGES, name='FILE_PROCESSING_STAGES'))
+	length = Column(Float) #length of observation in fraction of a day
+	currentpid = Column(Integer)
+	stillhost = Column(String(100))
+	stillpath = Column(String(100))
+	outputpath = Column(String(100))
+	outputhost = Column(String(100))
+	high_neighbors = relationship('Observation',
+									secondary=neighbors,
+									primaryjoin=obsnum==neighbors.c.low_neighbor_id,
+									secondaryjoin=obsnum==neighbors.c.high_neighbor_id,
+									backref='low_neighbors')
+
+class Neighbors(Base):
+	__tablename__ = 'neighbors'
+	low_neighbor_id = Column(BigInteger, ForeignKey('observation.obsnum'), unique=True),
+	high_neighbor_id = Column(BigInteger, ForeignKey('observation.obsnum'), unique=True)
+
+class File(Base):
+	__tablename__ = 'file'
+	filenum = Column(Integer, primary_key=True)
+	filename = Column(String(100))
+	host = Column(String(100))
+	obsnum=Column(BigInteger,ForeignKey('observation.obsnum'))
+	#this next line creates an attribute Observation.files which is the list of all
+	#  files associated with this observation
+	observation = relationship(Observation, backref=backref('files', uselist=True))
+	md5sum = Column(Integer)
+
+class Log(Base):
+	__tablename__ = 'log'
+	lognum = Column(Integer,primary_key=True)
+	obsnum = Column(BigInteger,ForeignKey('observation.obsnum'))
+	stage = Column(Enum(*FILE_PROCESSING_STAGES, name='FILE_PROCESSING_STAGES'))
+	exit_status = Column(Integer)
+	timestamp = Column(DateTime, nullable=False, default=func.current_timestamp())
+	logtext = Column(Text)
+
+class DataBaseInterface(ppdata.DataBaseInterface):
+	def __init__(self, configfile='~/paperdistiller.cfg'):
+		'''
+		Unique Interface for the paperdata database
+
+		Parameters
+		----------
+		configfile | str: paperdata database configuration file
+		'''
+		super(DataBaseInterface, self).__init__(configfile=configfile)
+
+	def add_entry_dict(self, s, TABLE, entry_dict):
+		'''
+		create a new entry.
+
+		Parameters
+		----------
+		s | object: session object
+		TABLE | str: table name
+		entry_dict | dict: dict of attributes for object
+		'''
+		super(DataBaseInterface, self).add_entry_dict(__name__, s, TABLE, entry_dict)
+
+	def get_entry(self, s, TABLE, unique_value):
+		'''
+		retrieves any object.
+		Errors if there are more than one of the same object in the db. This is bad and should
+		never happen
+
+		Parameters
+		----------
+		s | object: session object
+		TABLE | str: table name
+		unique_value | int/float/str: primary key value of row
+
+		Returns
+		-------
+		object: table object
+		'''
+		super(DataBaseInterface, self).get_entry(__name__, s, TABLE, unique_value)
--- paper/ganglia/scripts/monitor_folio_still.py
+++ paper/ganglia/scripts/monitor_folio_still.py
@@ -1,8 +1,8 @@
 #!/usr/global/paper/bin/python
 from __future__ import print_function
-import ddr_compress.dbi as ddbi
 from sqlalchemy import func
-import curses,time,os
+import curses, time, os
+from paper.distiller import dbi as ddbi
 from paper.ganglia import dbi as pyg
 
 #setup my output file
@@ -34,11 +34,11 @@ statusscr.nodelay(1)
 curline = 2
 colwidth = 50
 obslines = 20
-i=0
+i = 0
 stat = ['\\','|','/','-','.']
 try:
 	table = getattr(ddbi, 'Observation')
-	while(1):
+	while True:
 		timestamp = int(time.time())
 		log_info = []
 		#get the screen dimensions
@@ -46,113 +46,111 @@ try:
 		#load the currently executing files
 		i += 1
 		curline = 2
-		stdscr.addstr(0,30,stat[i%len(stat)])
-		s = dbi.Session()
-		totalobs = s.query(table).count()
-		stdscr.addstr(curline, 0, 'Number of observations currently in the database: {totalobs}'.format(totalobs=totalobs))
-		curline += 1
-		OBSs = s.query(table).filter(getattr(table, 'status') != 'NEW').filter(getattr(table, 'status') != 'COMPLETE').all()
-		#OBSs = s.query(table).all()
-		obsnums = [OBS.obsnum for OBS in OBSs]
-		stdscr.addstr(curline, 0, 'Number of observations currently being processed {num}'.format(num=len(obsnums)))
-		curline += 1
-		statusscr.erase()
-		statusscr.addstr(0, 0 ,'  ----  Still Idle  ----   ')
-		for j, obsnum in enumerate(obsnums):
-			try:
-				host, path, filename= dbi.get_input_file(obsnum)
-				status = dbi.get_obs_status(obsnum)
-				still_host = dbi.get_obs_still_host(obsnum)
-				current_pid = dbi.get_obs_pid(obsnum)
-			except:
-				host, path, filename = 'host', '/path/to/', 'zen.2345672.23245.uv'
-				status = 'WTF'
-			col = int(j/statusscr.getmaxyx()[0])
-			#print(col*colwidth)
-			if j == 0 or col == 0:
-				row = j
-			else:
-				row = j % statheight
-			try:
-				statusscr.addstr(row, col * colwidth,
-					'{filename} {status} {still_host}'.format(col=col, filename=os.path.basename(filename),
-																status=status, still_host=still_host))
-			except:
-				continue
-			#check for new filenames
-			path = os.path.dirname(filename)
-			file_name = os.path.basename(filename)
-			full_path = ':'.join(still_host, filename)
-			if filename not in file_pid.keys():
-				file_pid.update({filename:current_pid})
-				time_start = int(time.time())
-				file_start.update({filename:time_start})
-				file_end.update({filename:-1})
-			if file_pid[filename] not in [current_pid]:
-				time_end = int(time.time())
-				file_end.update({filename:time_end})
-				del_time = -1
-				full_stats = ''.join((full_path, status))
-				entry_dict = {'host':still_host,
-							'path':path,
-							'filename':file_name,
-							'full_path':full_path,
-							'status':status,
-							'full_stats':full_stats,
-							'del_time':del_time,
-							'time_start':file_start[filename],
-							'time_end':file_end[filename],
-							'timestamp':timestamp}
-				file_log.append(entry_dict)
-				file_pid.update({filename:current_pid})
-				time_start = int(time.time())
-				file_start.update({filename:time_start})
-				file_end.update({filename:-1})
-			if filename not in file_status.keys():
-				file_status.update({filename:status})
-				del_time = 0
-				full_stats = ''.join((full_path, status))
-				entry_dict = {'host':still_host,
-							'path':path,
-							'filename':file_name,
-							'full_path':full_path,
-							'status':status,
-							'full_stats':full_stats,
-							'del_time':del_time,
-							'time_start':file_start[filename],
-							'time_end':file_end[filename],
-							'timestamp':timestamp}
-				file_log.append(entry_dict)
-				file_time.update({filename:time.time()})
-			#write output log
-			if file_status[filename] not in [status]:
-				del_time = time.time() - file_time[filename]
-				full_stats = ''.join((full_path, status))
-				entry_dict = {'host':still_host,
-							'path':path,
-							'filename':file_name,
-							'full_path':full_path,
-							'status':status,
-							'full_stats':full_stats,
-							'del_time':del_time,
-							'time_start':file_start[filename],
-							'time_end':file_end[filename],
-							'timestamp':timestamp}
-				file_log.append(entry_dict)
-				file_status.update({filename:status})
-				file_time.update({filename:time.time()})
-		with pyg_dbi.session_scope as s:
-			for monitor_data in file_log:
-				pyg_dbi.add_entry_dict(s, 'Monitor', monitor_data)
-		file_log = []
-		s.close()
-		statusscr.refresh()
-		c = stdscr.getch()
-		if c == ord('q'):
-			break
-		time.sleep(1)
+		stdscr.addstr(0, 30, stat[i % len(stat)])
+		with ddbi.session_scope() as s:
+			totalobs = s.query(table).count()
+			stdscr.addstr(curline, 0, 'Number of observations currently in the database: {totalobs}'.format(totalobs=totalobs))
+			curline += 1
+			OBSs = s.query(table).filter(getattr(table, 'status') != 'NEW').filter(getattr(table, 'status') != 'COMPLETE').all()
+			#OBSs = s.query(table).all()
+			obs_len = len(OBSs)
+			stdscr.addstr(curline, 0, 'Number of observations currently being processed {num}'.format(num=obs_len))
+			curline += 1
+			statusscr.erase()
+			statusscr.addstr(0, 0 ,'  ----  Still Idle  ----   ')
+			for j, OBS in enumerate(OBSs):
+				try:
+					obsnum = getattr(OBS, 'obsnum')
+					FILE = dbi.get_entry(ddbi, s, 'file', obsnum)
+					host = getattr(FILE, 'host')
+					path, filename = os.path.split(getattr(FILE, 'filename'))
+					status = getattr(OBS, 'status')
+					still_host = getattr(OBS, 'stillhost')
+					current_pid = getattr(OBS, 'currentpid')
+				except:
+					host, path, filename = 'host', '/path/to/', 'zen.2345672.23245.uv'
+					status = 'WTF'
+				col = int(j / statusscr.getmaxyx()[0])
+				#print(col * colwidth)
+				if j == 0 or col == 0:
+					row = j
+				else:
+					row = j % statheight
+				try:
+					statusscr.addstr(row, col * colwidth,
+									'{filename} {status} {still_host}'.format(filename=filename, status=status, still_host=still_host))
+				except:
+					continue
+				#check for new filenames
+				full_path = ':'.join((still_host, filename))
+				if filename not in file_pid.keys():
+					file_pid.update({filename: current_pid})
+					time_start = int(time.time())
+					file_start.update({filename: time_start})
+					file_end.update({filename: -1})
+				if file_pid[filename] not in [current_pid]: 
+					time_end = int(time.time())
+					file_end.update({filename:time_end})
+					del_time = -1
+					full_stats = ''.join((full_path, status))
+					entry_dict = {'host': still_host,
+									'path': path,
+									'filename': filename,
+									'full_path': full_path,
+									'status': status,
+									'full_stats': full_stats,
+									'del_time': del_time,
+									'time_start': file_start[filename],
+									'time_end': file_end[filename],
+									'timestamp': timestamp}
+					file_log.append(entry_dict)
+					file_pid.update({filename: current_pid})
+					time_start = int(time.time())
+					file_start.update({filename: time_start})
+					file_end.update({filename: -1})
+				if filename not in file_status.keys(): 
+					file_status.update({filename: status})
+					del_time = 0
+					full_stats = ''.join((full_path, status))
+					entry_dict = {'host': still_host,
+									'path': path,
+									'filename': filename,
+									'full_path': full_path,
+									'status': status,
+									'full_stats': full_stats,
+									'del_time': del_time,
+									'time_start': file_start[filename],
+									'time_end': file_end[filename],
+									'timestamp': timestamp}
+					file_log.append(entry_dict)
+					file_time.update({filename: time.time()})
+				#write output log
+				if file_status[filename] not in [status]: 
+					del_time = time.time() - file_time[filename]
+					full_stats = ''.join((full_path, status))
+					entry_dict = {'host': still_host,
+								'path': path,
+								'filename': file_name,
+								'full_path': full_path,
+								'status': status,
+								'full_stats': full_stats,
+								'del_time': del_time,
+								'time_start': file_start[filename],
+								'time_end': file_end[filename],
+								'timestamp': timestamp}
+					file_log.append(entry_dict)
+					file_status.update({filename:status})
+					file_time.update({filename:time.time()})
+			with pyg_dbi.session_scope() as sess:
+				for monitor_data in file_log:
+					pyg_dbi.add_entry_dict(sess, 'Monitor', monitor_data)
+			file_log = []
+			statusscr.refresh()
+			c = stdscr.getch()
+			if c == ord('q'):
+				break
+			time.sleep(1)
 except(KeyboardInterrupt):
-	s.close()
 	pass
 #terminate
 curses.nocbreak(); stdscr.keypad(0); curses.echo()
