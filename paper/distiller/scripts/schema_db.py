5097e7c95f85b49ac0d7f4bbfdad543199e35060
yatsu <immwa@sas.upenn.edu>
2015-10-09 15:38:59 -0400

    name change

--- paper/distiller/scripts/schema_db.py
+++ paper/distiller/scripts/schema_db.py
@@ -1,6 +1,6 @@
 import os
 import paper.schema as schema
-from paper.distiller import distill_db as ddb
+from paper.distiller import distiller_db as ddb
 
 if __name__ == "__main__":
 	filename = os.path.expanduser('~/paperdata/paper/distiller/src/schema.txt')
