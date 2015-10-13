99e5655cffef7a8a33d6a05b6f7b57177b629af7
yatsu <immwa@sas.upenn.edu>
2015-10-13 16:23:32 -0400

    new template dir

--- paper/site/flask_app.py
+++ paper/site/flask_app.py
@@ -16,7 +16,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
-search_app = Flask(__name__)
+search_app = Flask(__name__, template_folder='search/templates')
 search_app.config.from_pyfile('search/settings.py')
 
 search_lm = LoginManager()
@@ -24,7 +24,7 @@ search_lm.init_app(search_app)
 
 search_db = SQLAlchemy(search_app)
 
-admin_app = Flask(__name__)
+admin_app = Flask(__name__, template_folder='admin/templates')
 admin_app.config.from_pyfile('admin/settings.py')
 
 admin_lm = LoginManager()
