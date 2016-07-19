ec197e276efb01bee9cc70b3dc9beeb30b12b1b2
immanuelw <immwa@sas.upenn.edu>
2016-07-19 00:10:49 -0400

    first step in fixing backup

---
+++ paper/backup.py
@@ -0,0 +1,59 @@
+'''
+paper.backup
+
+backups database into json files
+
+author | Immanuel Washington
+
+Functions
+---------
+backup | backs up database
+'''
+from __future__ import print_function
+import os
+import time
+import paper as ppdata
+from paper.data import dbi as pdbi
+from paper.distiller import dbi as ddbi
+from paper.ganglia import dbi as pyg
+
+def backup_db(db):
+    '''
+    backups database by loading into json files, named by timestamp
+
+    Parameters
+    ----------
+    db | str: name of database
+    '''
+    if db == 'paperdata':
+        xdbi = pdbi
+    elif db == 'paperdistiller':
+        xdbi = ddbi
+    elif db == 'ganglia'
+        xdbi = pyg
+
+    meta = xdbi.Base.metadata
+    dbi = xdbi.DataBaseInterface()
+
+    timestamp = int(time.time())
+    file_dir = os.path.dirname(__file__)
+    backup_dir = os.path.realpath(os.path.join(file_dir, '../../../data/{db}/{timestamp}'.format(db=db, timestamp=timestamp)))
+    if not os.path.isdir(backup_dir):
+        os.mkdir(backup_dir)
+
+    tables = {table.name: table.primary_key.columns.keys()[0] for table in meta.tables.values()}
+
+    with dbi.session_scope() as s:
+        print(timestamp)
+        for table, primary_key in tables.items():
+            db_file = '{table}_{timestamp}.json'.format(table=table.lower(), timestamp=timestamp)
+            backup_path = os.path.join(backup_dir, db_file)
+            print(db_file)
+
+            DB_table = getattr(xdbi, table)
+            DB_dump = s.query(DB_table).order_by(getattr(DB_table, primary_key).asc())
+            ew.json_data(backup_path, DB_dump)
+            print('Table data backup saved')
+
+if __name__ == '__main__':
+    print('This is just a module')
--- paper/data/scripts/backup_db.py
+++ paper/data/scripts/backup_db.py
@@ -1,54 +1,12 @@
 '''
 paper.data.scripts.backup_db
 
-backups paperdata database into json file
+backups paperdata database into json files
 
 author | Immanuel Washington
-
-Functions
----------
-json_data | dumps dictionaries to json file
-paperbackup | backs up paperdata database
 '''
 from __future__ import print_function
-import os
-import time
-import paper as ppdata
-from paper.data import dbi as pdbi
-
-def paperbackup(dbi, db):
-    '''
-    backups database by loading into json files, named by timestamp
-
-    Parameters
-    ----------
-    dbi | object: database interface object
-    db | str: name of directory to place backups into
-    '''
-    timestamp = int(time.time())
-    backup_dir = '/data4/paper/{db}_backup/{timestamp}'.format(db=db, timestamp=timestamp)
-    if not os.path.isdir(backup_dir):
-        os.mkdir(backup_dir)
-
-    #tables = ('Observation', 'File', 'Feed', 'Log')
-    tables = ('Observation', 'File', 'Log')
-    table_sorts = {'Observation': {'first': 'julian_date', 'second': 'polarization'},
-                   'File': {'first': 'obsnum', 'second': 'filename'},
-                   'Feed': {'first': 'julian_day', 'second': 'filename'},
-                   'Log': {'first': 'timestamp', 'second': 'action'}}
-    with dbi.session_scope() as s:
-        print(timestamp)
-        for table in tables:
-            db_file = '{table}_{timestamp}.json'.format(table=table.lower(), timestamp=timestamp)
-            backup_path = os.path.join(backup_dir, db_file)
-            print(db_file)
-
-            DB_table = getattr(pdbi, table)
-            DB_dump = s.query(DB_table).order_by(getattr(DB_table, table_sorts[table]['first']).asc(),
-                                                 getattr(DB_table, table_sorts[table]['second']).asc())
-            ppdata.json_data(backup_path, DB_dump)
-            print('Table data backup saved')
+from paper import backup
 
 if __name__ == '__main__':
-    dbi = pdbi.DataBaseInterface()
-    paperbackup(dbi, db='paperdata')
+    backup.backup_db(db='paperdata')
