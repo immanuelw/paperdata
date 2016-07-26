8b5556d8a9345568860a0a7784f786acdca33f11
immanuelw <immwa@sas.upenn.edu>
2016-07-25 20:20:11 -0400

    documentation

--- paper/site/flask_app.py
+++ paper/site/flask_app.py
@@ -5,10 +5,8 @@ author | Immanuel Washington
 
 Functions
 ---------
-search_app | creates flask app for search db
+search_app | creates flask app for search
 '''
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
 
 search_app = Flask(__name__, static_folder='search/static', template_folder='search/templates')
